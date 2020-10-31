e5d26bfc3527d529e3bf1ea16b261d692257431f
Pol Abellas, Rafael A <rafael.polabellas@dematic.com>
2020-10-30 20:56:10 -0400

    feat: adding cron job

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { CronJob } = require('cron');
+
 const configureAPI = require('./configure');
+const grantscraper = require('./lib/grantscraper');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 configureAPI(app);
-
 app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
+
+const job = new CronJob(
+    '* 30 * * * *',
+    grantscraper.run,
+);
+job.run();
