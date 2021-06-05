64d2a0276c2bd39e4052e7e508202d368d654d2b
Pol Abellas, Rafael A <rafael.polabellas@dematic.com>
2021-06-05 14:11:30 -0400

    fix: cannot delete users because of foreign constraint

---
+++ packages/server/migrations/20210605140431_access_token_foreign_user_id.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+    return knex.schema.table('access_tokens', (t) => {
+        t.dropForeign('user_id');
+        t.foreign('user_id').references('users.id').onDelete('CASCADE');
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.table('access_tokens', (t) => {
+        t.dropForeign('user_id');
+        t.foreign('user_id').references('users.id');
+    });
+};
