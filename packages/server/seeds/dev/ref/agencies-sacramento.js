7afa6463a5dbb3da1b777b230f239c70e818a8b8
Pol Abellas, Rafael A <rafael.polabellas@dematic.com>
2021-06-20 19:51:18 -0400

    feat: adding agencies from more states

---
+++ packages/server/seeds/dev/ref/agencies-sacramento.js
@@ -0,0 +1,85 @@
+module.exports = [
+    {
+        id: 0,
+        name: 'Office of the City Manager',
+    },
+    {
+        id: 1,
+        name: 'Cannabis Management',
+        parent: 0,
+    },
+    {
+        id: 2,
+        name: 'Climate Action & Sustainability',
+        parent: 0,
+    },
+    {
+        id: 3,
+        name: 'Diversity & Equity',
+        parent: 0,
+    },
+    {
+        id: 4,
+        name: 'Emergency Management',
+        parent: 0,
+    },
+    {
+        id: 5,
+        name: 'Innovation and Economic Development',
+        parent: 0,
+    },
+    {
+        id: 6,
+        name: 'Youth Development',
+        parent: 0,
+    },
+    {
+        id: 7,
+        name: 'Department of Community Development',
+    },
+    {
+        id: 8,
+        name: 'Department of Community Response',
+    },
+    {
+        id: 9,
+        name: 'Department of Convention and Cultural Services',
+    },
+    {
+        id: 10,
+        name: 'Department of Finance',
+    },
+    {
+        id: 11,
+        name: 'Fire Department',
+    },
+    {
+        id: 12,
+        name: 'Department of Human Resources',
+    },
+    {
+        id: 13,
+        name: 'Department of Information Technology',
+    },
+    {
+        id: 14,
+        name: 'Police Department',
+    },
+    {
+        id: 15,
+        name: 'Department of Public Works',
+    },
+    {
+        id: 16,
+        name: 'Department of Utilities',
+    },
+    {
+        id: 17,
+        name: 'Department of Youth, Parks, & Community Enrichment',
+    },
+    {
+        id: 18,
+        name: 'USDR',
+        abbreviation: 'USDR',
+    },
+];
---
+++ packages/server/seeds/dev/ref/agencies-topeka.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 0,
+        name: 'City of Topeka',
+        abbreviation: '',
+    },
+    {
+        id: 1,
+        name: 'IT',
+        abbreviation: '',
+    },
+    {
+        id: 2,
+        name: 'Planning/Housing',
+        abbreviation: '',
+    },
+    {
+        id: 3,
+        name: 'Public Works',
+        abbreviation: '',
+    },
+    {
+        id: 4,
+        name: 'Finance/Budget',
+        abbreviation: 'CRMC',
+    },
+    {
+        id: 5,
+        name: 'USDR',
+        abbreviation: 'USDR',
+    },
+];
