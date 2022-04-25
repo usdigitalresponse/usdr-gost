cfb35f8765d0846867fe069ea23ac2894d06b928
Matt Broussard <mbroussard@usdigitalresponse.org>
2022-04-25 13:28:51 -0700

    fix: handle 0 rows in CSV export

--- packages/server/src/routes/grants.js
+++ packages/server/src/routes/grants.js
@@ -108,12 +108,18 @@ router.get('/exportCSV', requireUser, async (req, res) => {
         award_floor: getAwardFloor(grant),
         url: `https://www.grants.gov/web/grants/view-opportunity.html?oppId=${grant.grant_id}`,
     }));
-    if (pagination.total > data.length) {
+
+    if (data.length === 0) {
+        // If there are 0 rows, csv-stringify won't even emit the header, resulting in a totally
+        // empty file, which is confusing. This adds a single empty row below the header.
+        formattedData.push({});
+    } else if (pagination.total > data.length) {
         formattedData.push({
             title: `Error: only ${MAX_CSV_EXPORT_ROWS} rows supported for CSV export, but there `
             + `are ${pagination.total} total.`,
         });
     }
+
     const csv = csvStringify(formattedData, {
         header: true,
         columns: [
