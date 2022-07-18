cc4f5c13dd59c37be538d3e3f5e65e30f8a7e44c
anigonzales <anisgonzales@gmail.com>
2022-07-18 11:27:10 -0500

    app.vue again

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
@@ -16,8 +15,7 @@ export default {
 <style lang="scss">
 @import 'bootstrap/scss/bootstrap.scss';
 @import 'bootstrap-vue/src/index.scss';
-@import "vue-select/src/scss/vue-select.scss";
-
+@import 'vue-select/src/scss/vue-select.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +25,3 @@ export default {
 body {
   font-size: .875rem;
 }
-
-</style>
