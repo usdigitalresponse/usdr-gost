cf677127de10c9ab55cee005ba21216a74a5d024
Claudius Mbemba <mbembac@gmail.com>
2022-08-03 11:41:55 -0700

    #165: fixing queries breaking staging (#205)
    * testing stagin fix
    
    * updated knex debug
    
    * added logging
    
    * discovered bug in knex - testing fix
    
    * added more logging
    
    * logging
    
    * updated logging
    
    * minor updates
    
    * minor updates
    
    * cleaned up logs
    
    * applying fix to interested grants
    
    * minor update
    
    * fixed lint err
    
    * removed check against USDR agency ID
    
    * added log
    
    * updated log
    
    * cleaned up comments & Query implementations

--- packages/server/src/routes/grants.js
+++ packages/server/src/routes/grants.js
@@ -19,25 +19,19 @@ const router = express.Router({ mergeParams: true });
  */
 async function getAgencyForUser(selectedAgency, user, { filterByMainAgency } = {}) {
     let agencies = [];
-    console.log('user:', JSON.stringify(user));
 
     if (selectedAgency === user.agency_id) {
         agencies = user.agency.subagencies;
-        console.log('agencies2:', agencies.length);
     }
-    console.log('agencies2.1:', agencies.length);
     if (agencies.length === 0) {
         if (filterByMainAgency && user.agency.main_agency_id) {
             // Get all agencies from the main agency. Usually the agency of the organization,
             // in other words the root parent agency (for example nevada agency)
             agencies = await db.getAgencies(user.agency.main_agency_id);
-            console.log('agencies3:', agencies.length);
         } else {
             agencies = await db.getAgencies(selectedAgency);
-            console.log('agencies4:', agencies.length);
         }
     }
-    console.log('agencies5:', agencies.length);
     return agencies.map((s) => s.id);
 }
 
@@ -67,7 +61,6 @@ router.get('/', requireUser, async (req, res) => {
     }
     const { selectedAgency, user } = req.session;
     const agencies = await getAgencyForUser(selectedAgency, user, { filterByMainAgency: true });
-    console.log('agencies6:', agencies.length);
 
     const grants = await db.getGrants({
         ...req.query,
