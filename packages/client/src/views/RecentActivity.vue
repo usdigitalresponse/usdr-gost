d578107faf179df50e083c2a3448e3bf99d48c54
anigonzales <anisgonzales@gmail.com>
2022-08-09 14:01:45 -0500

    recent activity ui

--- packages/client/src/views/RecentActivity.vue
+++ packages/client/src/views/RecentActivity.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="container">
-    <b-card title='Recent Activity' class="border-0">
+    <b-card class="border-0">
+      <h4 class="card-title gutter-title1 row">Recent Activity</h4>
     <b-table
       hover
       :items="activityItems"
@@ -14,6 +15,7 @@
       @row-selected="onRowSelected"
     >
       <template #cell(icon)="list">
+        <div class="gutter-icon row">
         <b-icon
           v-if="list.item.interested"
           icon="check-circle-fill"
@@ -21,15 +23,16 @@
           variant="success"
         ></b-icon>
         <b-icon v-else icon="x-circle-fill" scale="1" variant="danger"></b-icon>
+        </div>
       </template>
       <template #cell(agencyAndGrant)="agencies">
         <div>
           {{ agencies.item.agency }}
           <span v-if="agencies.item.interested">
-            is <span class="color-green">interested </span> in
+            is <span class="color-green"> <strong> interested </strong></span> in
           </span>
-          <span v-if="!agencies.item.interested" class="color-red">
-            rejected </span
+          <span v-if="!agencies.item.interested" class="color-red"><strong>
+            rejected </strong></span
           >{{ agencies.item.grant }}
         </div>
       </template>
@@ -49,14 +52,22 @@
 </template>
 <style scoped>
 .color-gray {
-  color: gray;
+  color: #757575
 }
 .color-red {
-  color: red;
+  color: #ae1818;
 }
 .color-green {
   color: green;
 }
+.gutter-icon.row {
+    margin-right: -8px;
+    margin-left: -8px;
+    margin-top: 3px;
+  }
+   .gutter-title1.row {
+    margin-left: +4px;
+  }
 </style>
 
 <script>
@@ -114,7 +125,14 @@ export default {
         grant_id: grantsInterested.grant_id,
         interested: !grantsInterested.is_rejection,
         dateSort: new Date(grantsInterested.created_at).toLocaleString(),
-        date: rtf.format(Math.round((new Date(grantsInterested.created_at).getTime() - new Date().getTime()) / oneDayInMs), 'day').charAt(0).toUpperCase() + rtf.format(Math.round((new Date(grantsInterested.created_at).getTime() - new Date().getTime()) / oneDayInMs), 'day').slice(1),
+        date: (() => {
+          const timeSince = rtf.format(Math.round((new Date(grantsInterested.created_at).getTime() - new Date().getTime()) / oneDayInMs), 'day');
+          const timeSinceInt = parseInt(timeSince, 10);
+          if (!Number.isNaN(timeSinceInt) && timeSinceInt > 7) {
+            return new Date(grantsInterested.created_at).toLocaleDateString('en-US');
+          }
+          return timeSince.charAt(0).toUpperCase() + timeSince.slice(1);
+        })(),
       }));
     },
     totalRows() {
