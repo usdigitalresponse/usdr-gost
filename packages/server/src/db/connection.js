3be65f37353a45fc4c320a48b000da952c22041f
Claudius Mbemba <mbembac@gmail.com>
2022-08-08 18:07:18 -0700

    updated knex setup

--- packages/server/src/db/connection.js
+++ packages/server/src/db/connection.js
@@ -1,19 +1,7 @@
-// const db = process.env.NODE_ENV !== 'test' ? process.env.POSTGRES_URL : (process.env.POSTGRES_TEST_URL || 'postgresql://localhost:5432/usdr_grants_test');
 const { attachPaginate } = require('knex-paginate');
-const config = require('./knexfile');
+const config = require('../../knexfile');
 // eslint-disable-next-line import/order
 const knex = require('knex')(config[process.env.NODE_ENV || 'production']);
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: db,
-//     seeds: {
-//         directory: './seeds',
-//     },
-//     migrations: {
-//         directory: './migrations',
-//     },
-//     debug: process.env.NODE_ENV === 'staging',
-// });
 
 attachPaginate();
 
