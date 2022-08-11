9d24486b143f6e4fe2da570211ffeacecf176e1d
Clayton Gowan <cgowan944@icloud.com>
2022-08-11 13:01:08 -0500

    make upcoming dates entries show only those interested by selected agency or children

--- packages/server/src/db/index.js
+++ packages/server/src/db/index.js
@@ -377,13 +377,15 @@ async function getSingleGrantDetails({ grantId, agencies }) {
     };
 }
 
-async function getClosestGrants() {
+async function getClosestGrants(agency) {
+    const userAgencies = await getAgencies(agency);
     const timestamp = new Date().toLocaleDateString('en-US');
     const query = await knex(TABLES.grants)
         .select('title', 'close_date', 'grant_id')
         .where('close_date', '>=', timestamp)
         .whereIn('grant_id', function () {
-            this.select('grant_id').from('grants_interested');
+            this.select('grant_id').from('grants_interested')
+            .where('agency_id', 'IN', userAgencies.map((subAgency) => subAgency.id));
         })
         .orderBy('close_date', 'asc')
         .limit(3)
