33dba5e618353d318c3d79d799d33062f79cea5c
Brian Spates <b.spates@gmail.com>
2022-08-10 17:23:04 -0700

    Fix whitespace, and disable nullables for main_agency_id fields in tenant and agency

--- packages/server/migrations/20220810231630_enforce_tenancy.js
+++ packages/server/migrations/20220810231630_enforce_tenancy.js
@@ -1,16 +1,22 @@
 
 exports.up = function(knex) {
-  return knex.schema.alterTable('users', function(table) {
-    table.dropNullable('tenant_id');
-  }).alterTable('agencies', function(table) {
-    table.dropNullable('tenant_id');
-  });
+    return knex.schema.alterTable('users', function(table) {
+        table.dropNullable('tenant_id');
+    }).alterTable('agencies', function(table) {
+        table.dropNullable('tenant_id');
+        table.dropNullable('main_agency_id');
+    }).alterTable('tenants', function(table) {
+        table.dropNullable('main_agency_id');
+    });
 };
 
 exports.down = function(knex) {
-  return knex.schema.alterTable('users', function(table) {
-    table.setNullable('tenant_id');
-  }).alterTable('agencies', function(table) {
-    table.setNullable('tenant_id');
-  });
+    return knex.schema.alterTable('users', function(table) {
+        table.setNullable('tenant_id');
+    }).alterTable('agencies', function(table) {
+        table.setNullable('tenant_id');
+        table.setNullable('main_agency_id');
+    }).alterTable('tenants', function(table) {
+        table.setNullable('main_agency_id');
+    });
 };
