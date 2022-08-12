c058a5129139322cb4cc12ecc5d4310eb9cbbeea
Claudius Mbemba <mbembac@gmail.com>
2022-08-12 16:15:09 -0700

    minor updates

--- packages/server/__tests__/api/fixtures.js
+++ packages/server/__tests__/api/fixtures.js
@@ -7,11 +7,11 @@ async function mochaGlobalSetup() {
     }
 }
 
-// https://gist.github.com/boneskull/7fe75b63d613fa940db7ec990a5f5843
 async function mochaGlobalTeardown() {
     // kill running server process
     execShellCommand(`pkill -fn 'node src'`, { env: process.env });
-    // process.exit(0);  // forces tests to pass even if there are failures
+    // process.exit(0);
+    // forces tests to pass even if there are failures best to use https://gist.github.com/boneskull/7fe75b63d613fa940db7ec990a5f5843
 }
 
 module.exports = {
