071cb260fda8e00a6a31cb532cdb021e63fa78a7
Claudius Mbemba <mbembac@gmail.com>
2022-08-13 10:43:39 -0700

    test ci fix

--- packages/server/__tests__/api/fixtures.js
+++ packages/server/__tests__/api/fixtures.js
@@ -10,7 +10,7 @@ async function mochaGlobalSetup() {
 async function mochaGlobalTeardown() {
     // kill running server process
     execShellCommand(`pkill -fn 'node src'`, { env: process.env });
-    // process.exit(0);
+    process.exit(0);
     // forces tests to pass even if there are failures best to use https://gist.github.com/boneskull/7fe75b63d613fa940db7ec990a5f5843
 }
 
