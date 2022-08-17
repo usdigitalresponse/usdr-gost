7617f8d50916aeb44d3800af6df9bc1b7c604a76
Brian Spates <b.spates@gmail.com>
2022-08-17 11:48:29 -0700

    undo non nullable field that creates cyclical foriegn dependency, and update tests for tenancy

--- packages/server/migrations/20220810231630_enforce_tenancy.js
+++ packages/server/migrations/20220810231630_enforce_tenancy.js
@@ -5,8 +5,6 @@ exports.up = function (knex) {
     }).alterTable('agencies', (table) => {
         table.dropNullable('tenant_id');
         table.dropNullable('main_agency_id');
-    }).alterTable('tenants', (table) => {
-        table.dropNullable('main_agency_id');
     });
 };
 
@@ -15,8 +13,6 @@ exports.down = function (knex) {
         table.setNullable('tenant_id');
     }).alterTable('agencies', (table) => {
         table.setNullable('tenant_id');
-        table.setNullable('main_agency_id');
-    }).alterTable('tenants', (table) => {
         table.setNullable('main_agency_id');
     });
 };
