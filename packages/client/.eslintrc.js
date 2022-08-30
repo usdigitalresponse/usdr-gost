6420a9717eb7c6c57a4c2028ef8014582bfb1d62
Matt Broussard <mbroussard@usdigitalresponse.org>
2022-08-29 17:01:09 -0700

    chore: disable func-names linter on client (already done for server in #295

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     // }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'func-names': 'off',
   },
   overrides: [
     {
