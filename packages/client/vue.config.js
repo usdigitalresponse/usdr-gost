0a75febec3b6ba8307a5dbcf4817bdf7a8254fdf
Matt Broussard <mbroussard@usdigitalresponse.org>
2022-08-31 22:20:30 -0700

    chore: vue/webpack: multiple entry points

--- packages/client/vue.config.js
+++ packages/client/vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  pages: {
+    main: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+    // arpaReporter: {
+    //   entry: 'src/arpa_reporter/index.js',
+    //   template: 'public/arpa_reporter/index.html',
+    //   filename: 'arpa_reporter/index.html',
+    // },
+  },
   devServer: {
     client: {
       progress: false,
