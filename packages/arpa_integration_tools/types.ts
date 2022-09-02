f217af1c73e04fccf70f4857669abfbeb1a34258
Matt Broussard <mbroussard@usdigitalresponse.org>
2022-09-01 21:31:36 -0700

    chore: add some comments

--- packages/arpa_integration_tools/types.ts
+++ packages/arpa_integration_tools/types.ts
@@ -1,8 +1,17 @@
 export interface Config {
+  // Path of the source repo where things will be copied from
   srcPath: string;
+  // Name of the source repo (used in footer comment added to copied files)
   srcRepoName: string;
+  // Path of the destination repo where things will be copied to
   destPath: string;
+  // Describes which files should be copied where. The key is a glob pattern of
+  // paths to grab, and the value is path where the file(s)/folder(s) should be copied
+  // to, optionally with additional configuration (see CopyConfig).
   copies: { [oldPathGlob: string]: CopyConfig | string /* new path */ };
+  // Map of file paths in the source repo that should be rewritten to different paths
+  // in the destination repo. These should be relative to srcPath/destPath and have file
+  // extensions.
   importRewrites: { [oldPath: string]: string /* new path */ };
 }
 
