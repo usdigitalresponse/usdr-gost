b0cedad33501fe306afdc6c0ec9bc4e435616495
Tyler Hendrickson <hendrickson.tsh@gmail.com>
2022-10-03 12:53:35 -0500

    426: Add tooling for docker-based development environments (#427)
    * Define Dockerfile build instructions for common, client, and api containers
    
    * Configure Docker Compose for dev setup
    
    * Conditionally set Vue `devServer` config opts with env vars
    
    * Add README for Docker-based development (`docker/README.md`)

---
+++ docker/postgres/docker-entrypoint-preinitdb.d/example.sh
@@ -0,0 +1,4 @@
+# Any file in this directory with a .sh extension will be executed within the postgres
+# container before postgresql is initialized or started.
+#
+# This file is simply a placeholder and does not do anything.
