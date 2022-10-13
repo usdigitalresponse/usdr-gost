f9b966e8edd13ede07c8d16799965d1ce6d95f90
rancerun <lancesun22@gmail.com>
2022-10-12 21:09:11 -0400

    add migration to add new status code column in interested code along with new values and deleting the old dependency

---
+++ packages/server/migrations/20221012195737_update_interested_codes_is_rejection_column.js
@@ -0,0 +1,46 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    // Create a new column as grant statuses are no longer binary and a boolean will no longer be sufficient
+    await knex.schema.table('interested_codes', (table) => { table.string('status_code'); });
+
+    // Copying existing is_rejection column values over to the new column
+    await knex('interested_codes').where({ is_rejection: false }).update({ status_code: 'Interested' });
+    await knex('interested_codes').where({ is_rejection: true }).update({ status_code: 'Rejected' });
+
+    // Add new grant statuses and set the auto_increment counter for id to the accurate number as
+    // knex was trying to add new entries at id=1
+    await knex.raw('SELECT setval(\'interested_codes_id_seq\', max(id)) FROM interested_codes');
+    await knex('interested_codes').insert([
+        {
+            name: 'Applied',
+            status_code: 'Result',
+        },
+        {
+            name: 'Awarded',
+            status_code: 'Result',
+        },
+        {
+            name: 'Rejected',
+            status_code: 'Result',
+        },
+    ]);
+
+    await knex.schema.table('interested_codes', (table) => { table.dropColumn('is_rejection'); });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.table('interested_codes', (table) => { table.boolean('is_rejection'); });
+
+    await knex('interested_codes').where({ status_code: 'Interested' }).update({ is_rejection: false });
+    await knex('interested_codes').where({ status_code: 'Rejected' }).update({ is_rejection: true });
+    await knex('interested_codes').whereIn('name', ['Applied', 'Awarded', 'Rejected']).del();
+
+    await knex.schema.table('interested_codes', (table) => { table.dropColumn('status_code'); });
+};
