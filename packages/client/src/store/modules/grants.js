d7dd3fbcabdd790c4fa9c6f4376b5d4b851e8997
chrisxkeith <chris.keith@gmail.com>
2022-10-24 08:42:06 -0700

    chore: resolve merge conflicts

--- packages/client/src/store/modules/grants.js
+++ packages/client/src/store/modules/grants.js
@@ -32,10 +32,10 @@ export default {
   },
   actions: {
     fetchGrants({ commit }, {
-      currentPage, perPage, orderBy, orderDesc, searchTerm, interestedByMe, assignedToAgency, aging, positiveInterest, rejected, interestedByAgency,
+      currentPage, perPage, orderBy, orderDesc, searchTerm, interestedByMe, assignedToAgency, aging, positiveInterest, result, rejected, interestedByAgency,
     }) {
       const query = Object.entries({
-        currentPage, perPage, orderBy, orderDesc, searchTerm, interestedByMe, assignedToAgency, aging, positiveInterest, rejected, interestedByAgency,
+        currentPage, perPage, orderBy, orderDesc, searchTerm, interestedByMe, assignedToAgency, aging, positiveInterest, result, rejected, interestedByAgency,
       })
         // filter out undefined and nulls since api expects parameters not present as undefined
         // eslint-disable-next-line no-unused-vars
