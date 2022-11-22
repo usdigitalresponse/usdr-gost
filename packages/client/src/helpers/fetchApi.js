375d4aba87573d0b4368ecdb8b3ce0d3754b4bc1
TylerHendrickson <hendrickson.tsh@gmail.com>
2022-11-21 20:43:25 -0600

    Determine API URL from window directive injected at runtime

--- packages/client/src/helpers/fetchApi.js
+++ packages/client/src/helpers/fetchApi.js
@@ -2,7 +2,7 @@ import store from '@/store';
 import urlJoin from 'url-join';
 
 export function apiURL(endpointPath) {
-  const baseURL = process.env.GOST_API_URL || '/';
+  const baseURL = window.apiURLForGOST || '/';
   return urlJoin(baseURL, endpointPath);
 }
 
