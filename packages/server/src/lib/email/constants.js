45f2bb29b10b7f52cb781d47c419e8a23e2e0227
aditya <asridhar@usdigitalresponse.org>
2023-01-04 13:45:14 -0500

    feat: adds table to store subscription data (#763)

---
+++ packages/server/src/lib/email/constants.js
@@ -0,0 +1,12 @@
+const notificationType = {
+    grantAssignment: 'GRANT_ASSIGNMENT',
+    grantInterest: 'GRANT_INTEREST',
+    grantDigest: 'GRANT_DIGEST',
+};
+
+const emailSubscriptionStatus = {
+    subscribed: 'SUBSCRIBED',
+    unsubscribed: 'UNSUBSCRIBED',
+};
+
+module.exports = { notificationType, emailSubscriptionStatus };
