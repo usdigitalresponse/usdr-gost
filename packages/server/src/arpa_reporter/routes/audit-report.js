f2c4be259e11b7c17dfaf02092c909e0d232295f
Andrew Hyndman <ajhyndman@hotmail.com>
2023-01-19 20:35:22 -0800

    Fix a typo in audit report console logging (#841)

--- packages/server/src/arpa_reporter/routes/audit-report.js
+++ packages/server/src/arpa_reporter/routes/audit-report.js
@@ -15,7 +15,7 @@ router.get('/', requireUser, async function (req, res) {
     console.log('Successfully generated report');
   } catch (error) {
     // In addition to sending the error message in the 500 response, log the full error stacktrace
-    console.log(`Audit report generation failed. Logging the thrown error.`, e)
+    console.log(`Audit report generation failed. Logging the thrown error.`, error)
     return res.status(500).send(error.message)
   }
 
