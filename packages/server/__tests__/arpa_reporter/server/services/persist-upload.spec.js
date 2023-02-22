e11f9052e6036ff103f231843251261fe90a10e4
Nat Hillard <nat.hillard.usdr@gmail.com>
2023-02-22 00:18:23 -0500

    feat: support freeform text notes on uploads (#896)
    Adding tests for persist-upload

--- packages/server/__tests__/arpa_reporter/server/services/persist-upload.spec.js
+++ packages/server/__tests__/arpa_reporter/server/services/persist-upload.spec.js
@@ -1,25 +1,97 @@
-const { expect } = require('chai')
-const { mock } = require('sinon')
-const fs = require('fs/promises')
+const { expect } = require('chai');
+const sinon = require('sinon');
+const fs = require('fs/promises');
+const path = require('path');
+const rewire = require('rewire');
+const ValidationError = require('../../../../src/arpa_reporter/lib/validation-error');
+const { UPLOAD_DIR } = require('../../../../src/arpa_reporter/environment');
 
 const XLSX = require('xlsx')
-const {
-  _uploadFSName,
-  _jsonFSName,
-  _persistJson,
-  _jsonForUpload,
-  persistUpload,
-  workbookForUpload,
-} = require('../../../../src/arpa_reporter/services/persist-upload')
+const persistUpload = rewire('../../../../src/arpa_reporter/services/persist-upload');
+
+// These are used in multiple places
+const jsonFSName = persistUpload.__get__('jsonFSName');
+const persistJson = persistUpload.__get__('persistJson');
+const uploadFSName = persistUpload.__get__('uploadFSName');
 
 const MOCK_WORKBOOK = XLSX.utils.book_new();
 XLSX.utils.book_append_sheet(MOCK_WORKBOOK, XLSX.utils.aoa_to_sheet([[0, 1, 2]]), 'sheet_1');
 
 describe('persist-upload', () => {
+  describe('validateBuffer', () => {
+    const validateBuffer = persistUpload.__get__('validateBuffer');
+  
+    it('should return true for a valid excel file', async () => {
+      const buffer = await fs.readFile(path.join(__dirname, "../fixtures/arpa-examples/OBM01-768622-09302020-v20220320.xlsm"));
+      const result = await validateBuffer(buffer);
+      expect(result).not.to.throw;
+    });
+  
+    it('should throw an error for an invalid buffer', async () => {
+      const buffer = Buffer.from([0x01]);
+      expect(validateBuffer(buffer)).to.throw;
+    });
+  });
+
+  describe('createUploadRow', () => {
+    const createUploadRow = persistUpload.__get__('createUploadRow');
+  
+    it('should create an upload row with escaped notes', async () => {
+      const body = { notes: '<script>alert("xss");</script>' };
+      const user = { id: 1 };
+      const reportingPeriod = { id: 2 };
+      const uploadRow = await createUploadRow('filename.xlsx', reportingPeriod, user, body);
+      expect(uploadRow.filename).to.equal('filename.xlsx');
+      expect(uploadRow.reporting_period_id).to.equal(2);
+      expect(uploadRow.user_id).to.equal(1);
+      expect(uploadRow.notes).to.equal('&lt;script&gt;alert(&quot;xss&quot;);&lt;/script&gt;');
+    });
+  });  
+
+
+  describe('persistUploadToFS', () => {
+    const persistUploadToFS = persistUpload.__get__("persistUploadToFS");
+    let fsMock;
+    const filename = 'test.xlsx';
+    const upload = { filename: filename, id: 1 };
+    const filePath = uploadFSName(upload);
+    const buffer = Buffer.from('test data');
+
+    beforeEach(() => {
+      fsMock = sinon.mock(fs);
+    });
+
+    afterEach(() => {
+      fsMock.restore();
+    });
+
+    it('should write the file to disk', async () => {
+      fsMock.expects('mkdir').once().withArgs(UPLOAD_DIR, { recursive: true }).resolves();
+      fsMock.expects('writeFile').once().withArgs(filePath, buffer, { flag: 'wx' }).resolves();
+  
+      await persistUploadToFS(upload, buffer);
+  
+      fsMock.verify();
+    });
+
+    it('should throw a ValidationError if an error occurs', async () => {
+      const expectedError = new ValidationError(`Cannot persist ${upload.filename} to filesystem: Error: EEXIST: file already exists`);
+      fsMock.expects('mkdir').throws(new Error("EEXIST: file already exists"));
+    
+      try {
+        await persistUploadToFS(upload, buffer);
+        throw new Error('Expected an error to be thrown');
+      } catch (err) {
+        expect(err).to.be.an.instanceof(ValidationError);
+        expect(err.message).to.equal(expectedError.message);
+      } 
+    });
+  });
+
   describe('uploadFSName', () => {
     it('generates a filesystem name based on upload ID', () => {
       expect(
-        _uploadFSName({
+        uploadFSName({
           id: 'abcd',
           filename: 'upload01.xlsm',
         }),
@@ -29,16 +101,16 @@ describe('persist-upload', () => {
 
   describe('jsonFSName', () => {
     it('generates a filesystem name based on upload ID', () => {
-      expect(_jsonFSName({ id: 'abcd', filename: 'upload01.xlsm' })).include(
+      expect(jsonFSName({ id: 'abcd', filename: 'upload01.xlsm' })).include(
         '/abcd.json',
       )
     })
 
     it('groups cache contents in subfolders by first character of upload ID', () => {
-      expect(_jsonFSName({ id: 'abcd', filename: 'upload01.xlsm' })).include(
+      expect(jsonFSName({ id: 'abcd', filename: 'upload01.xlsm' })).include(
         '/a/abcd.json',
       )
-      expect(_jsonFSName({ id: '01234', filename: 'upload02.xlsm' })).include(
+      expect(jsonFSName({ id: '01234', filename: 'upload02.xlsm' })).include(
         '/0/01234.json',
       )
     })
@@ -47,7 +119,7 @@ describe('persist-upload', () => {
   describe('persistJson', () => {
     let fsMock
     beforeEach(() => {
-      fsMock = mock(fs)
+      fsMock = sinon.mock(fs)
     })
 
     afterEach(() => {
@@ -57,25 +129,27 @@ describe('persist-upload', () => {
     it('serializes a workbook object and saves it to disk', async () => {
       const upload = { id: 'abcd', filename: 'upload01.xlsm' }
       fsMock.expects('mkdir').once()
-      fsMock.expects('writeFile').once().withArgs(_jsonFSName(upload))
-      await _persistJson(upload, MOCK_WORKBOOK)
+      fsMock.expects('writeFile').once().withArgs(jsonFSName(upload))
+      await persistJson(upload, MOCK_WORKBOOK)
       fsMock.verify()
     })
   })
 
   describe('jsonForUpload', () => {
+    const jsonForUpload = persistUpload.__get__('jsonForUpload');
+
     it('serializing and deserializing an upload are reversible', async () => {
       const upload = { id: 'abcd', filename: 'upload01.xlsm' }
-      await _persistJson(upload, MOCK_WORKBOOK)
-      const workbook = await _jsonForUpload(upload)
+      await persistJson(upload, MOCK_WORKBOOK)
+      const workbook = await jsonForUpload(upload)
       expect(workbook).deep.equal(MOCK_WORKBOOK)
     })
 
     it('preserves Date objects', async () => {
       const upload = { id: 'efgh', filename: 'upload02.xlsm' }
       const mockWorkbook = { ...MOCK_WORKBOOK, date: new Date(0) }
-      await _persistJson(upload, mockWorkbook)
-      const workbook = await _jsonForUpload(upload)
+      await persistJson(upload, mockWorkbook)
+      const workbook = await jsonForUpload(upload)
       expect(workbook).deep.equal(mockWorkbook)
       expect(workbook.date instanceof Date).is.true
     })
--- packages/server/src/arpa_reporter/services/persist-upload.js
+++ packages/server/src/arpa_reporter/services/persist-upload.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const fs = require('fs/promises')
+const _ = require('lodash');
 
 const Cryo = require('cryo')
 const XLSX = require('xlsx')
@@ -26,39 +27,53 @@ const jsonFSName = (upload) => {
   return path.join(TEMP_DIR, upload.id[0], filename)
 }
 
-async function persistUpload ({ filename, user, buffer, body }) {
-  // let's make sure we can actually read the supplied buffer (it's a valid spreadsheet)
+
+async function validateBuffer(buffer) {
   try {
-    await XLSX.read(buffer, { type: 'buffer' })
+    await XLSX.read(buffer, { type: 'buffer' });
   } catch (e) {
-    throw new ValidationError(`Cannot parse XLSX from data in ${filename}: ${e}`)
+    throw new ValidationError(`Cannot parse XLSX from supplied data: ${e}`);
   }
+}
 
-  // get the current reporting period
-  const reportingPeriod = await getReportingPeriod()
+function createUploadRow(filename, reportingPeriod, user, body) {
   const escapedNotes = _.escape(body.notes);
-
-  // create an upload
-  const uploadRow = {
+  return {
     filename: path.basename(filename),
     reporting_period_id: reportingPeriod.id,
     user_id: user.id,
-    notes: escapedNotes ?? null
-  }
+    notes: escapedNotes ?? null,
+  };
+}
 
-  const upload = await createUpload(uploadRow)
-
-  // persist the original upload to the filesystem
+async function persistUploadToFS(upload, buffer) {
   try {
-    const filename = uploadFSName(upload)
-    await fs.mkdir(path.dirname(filename), { recursive: true })
-    await fs.writeFile(filename, buffer, { flag: 'wx' })
+    const filename = uploadFSName(upload);
+    await fs.mkdir(path.dirname(filename), { recursive: true });
+    await fs.writeFile(filename, buffer, { flag: 'wx' });
   } catch (e) {
-    throw new ValidationError(`Cannot persist ${upload.filename} to filesystem: ${e}`)
+    throw new ValidationError(`Cannot persist ${upload.filename} to filesystem: ${e}`);
   }
+}
 
-  // return the upload we created
-  return upload
+async function persistUpload ({ filename, user, buffer, body }) {
+  // Make sure we can actually read the supplied buffer (it's a valid spreadsheet)
+  await validateBuffer(buffer);
+
+  // Get the current reporting period
+  const reportingPeriod = await getReportingPeriod();
+
+  // Create the upload row
+  const uploadRow = createUploadRow(filename, reportingPeriod, user, body);
+
+  // Create the upload
+  const upload = await createUpload(uploadRow);
+
+  // Persist the upload to the filesystem
+  await persistUploadToFS(upload, buffer);
+
+  // Return the upload we created
+  return upload;
 }
 
 async function persistJson (upload, workbook) {
@@ -117,12 +132,6 @@ module.exports = {
   bufferForUpload,
   workbookForUpload,
   uploadFSName,
-
-  // exported for test purposes only!
-  _uploadFSName: uploadFSName,
-  _jsonFSName: jsonFSName,
-  _persistJson: persistJson,
-  _jsonForUpload: jsonForUpload,
 }
 
 // NOTE: This file was copied from src/server/services/persist-upload.js (git @ ada8bfdc98) in the arpa-reporter repo on 2022-09-23T20:05:47.735Z
