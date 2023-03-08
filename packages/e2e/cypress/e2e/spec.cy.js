5d0977249bcddec98aa54a02afbae9afbce9634a
Aditya Sridhar <aditus06@gmail.com>
2023-03-08 11:55:25 -0500

    fix: linting

--- packages/e2e/cypress/e2e/spec.cy.js
+++ packages/e2e/cypress/e2e/spec.cy.js
@@ -2,8 +2,9 @@ it('loads page', () => {
     cy.visit('/', { timeout: 30000 });
 
     cy.on('uncaught:exception', (err, runnable) => {
+        console.log(runnable);
         // This error is due to a lack of deploy-config.js file in the local environment.
-        expect(err.message).to.include('token')
+        expect(err.message).to.include('token');
 
         // using mocha's async done callback to finish
         // this test so we prove that an uncaught exception
@@ -11,8 +12,8 @@ it('loads page', () => {
 
         // return false to prevent the error from
         // failing this test
-        return false
-    })
+        return false;
+    });
 
     cy.contains('Grants Identification Tool');
 });
