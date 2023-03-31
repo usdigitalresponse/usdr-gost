82ba56f63313dd53b71c4ae5917a869df3994720
aditya <asridhar@usdigitalresponse.org>
2023-03-31 11:55:21 -0400

    feat: adds an s3 bucket to store arpa reporting data (#1154)

--- terraform/modules/gost_api/storage.tf
+++ terraform/modules/gost_api/storage.tf
@@ -23,3 +23,49 @@ module "efs_data_volume" {
     }
   }
 }
+
+module "arpa_audit_reports_bucket" {
+  source  = "cloudposse/s3-bucket/aws"
+  version = "3.0.0"
+  context = module.s3_label.context
+  name    = "arpa_audit_reports"
+
+  acl                          = "private"
+  versioning_enabled           = true
+  sse_algorithm                = "AES256"
+  allow_ssl_requests_only      = true
+  allow_encrypted_uploads_only = true
+  source_policy_documents      = []
+
+  lifecycle_configuration_rules = [
+    {
+      enabled                                = true
+      id                                     = "rule-1"
+      filter_and                             = null
+      abort_incomplete_multipart_upload_days = 1
+      expiration                             = { days = 14 }
+      transition                             = null
+      noncurrent_version_expiration          = { days = 7 }
+      noncurrent_version_transition          = null
+    }
+  ]
+}
+
+module "access_arpa_reports_bucket_policy" {
+  source  = "cloudposse/iam-policy/aws"
+  version = "0.4.0"
+  context = module.s3_label.context
+
+  name = "access_arpa_reports_bucket"
+
+  iam_policy_statements = {
+    ReadWriteBucketObjects = {
+      effect = "Allow"
+      actions = [
+        "s3:PutObject",
+        "s3:GetObject",
+      ]
+      resources = ["${module.arpa_audit_reports_bucket.bucket_arn}/*"]
+    }
+  }
+}
