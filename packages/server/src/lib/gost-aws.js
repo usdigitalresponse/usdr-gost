5ec57304d7675432482c22e8602300debc163e1c
M. Hilton <meredithjunior@gmail.com>
2023-04-06 11:07:34 -0400

    adding SES creation to new file

--- packages/server/src/lib/gost-aws.js
+++ packages/server/src/lib/gost-aws.js
@@ -1,5 +1,11 @@
 const AWS = require('aws-sdk');
 
+/*
+----------------------------------------------------------
+                        AWS S3
+----------------------------------------------------------
+*/
+
 function getS3Client() {
     let s3;
     if (process.env.LOCALSTACK_HOSTNAME) {
@@ -24,6 +30,69 @@ function getS3Client() {
     return s3;
 }
 
+/*
+----------------------------------------------------------
+                        AWS SES
+----------------------------------------------------------
+*/
+
+function createTransport() {
+    const requiredEnvironmentVariables = [
+        'NOTIFICATIONS_EMAIL',
+    ];
+    for (let i = 0; i < requiredEnvironmentVariables.length; i += 1) {
+        const ev = process.env[requiredEnvironmentVariables[i]];
+        if (!ev) {
+            return {
+                sendEmail: () => {
+                    throw new Error(
+                        `Missing environment variable ${requiredEnvironmentVariables[i]}!`,
+                    );
+                },
+            };
+        }
+    }
+
+    const sesOptions = {};
+    if (process.env.SES_REGION) {
+        sesOptions.region = process.env.SES_REGION;
+    }
+
+    return new AWS.SES(sesOptions);
+}
+
+function send(message) {
+    if (process.env.SUPPRESS_EMAIL) return;
+
+    const transport = createTransport();
+    const params = {
+        Destination: {
+            ToAddresses: [message.toAddress],
+        },
+        Source: process.env.NOTIFICATIONS_EMAIL,
+        Message: {
+            Subject: {
+                Charset: 'UTF-8',
+                Data: message.subject,
+            },
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: message.body,
+                },
+                Text: {
+                    Charset: 'UTF-8',
+                    Data: message.text,
+                },
+            },
+        },
+    };
+    transport.sendEmail(params).promise()
+        .then((data) => console.log('Success sending SES email:', data))
+        .catch((err) => console.error('Error sending SES email:', err, err.stack));
+}
+
 module.exports = {
     getS3Client,
+    send
 };
