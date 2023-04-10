8ba26ab2900aee1ced57c136b41e291683cb0435
Jack Pearkes <jackpearkes@gmail.com>
2023-04-10 09:23:07 -0700

    db: add migration to drop agencies.main_agency_id

---
+++ packages/server/migrations/20230410161411_drop_agencies_agency_main_id.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+        .table('agencies', (table) => {
+            table.dropColumn('main_agency_id');
+        });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+        .table('agencies', (table) => {
+            table.integer('main_agency_id').unsigned();
+            table.foreign('main_agency_id').references('agencies.id');
+            table.setNullable('main_agency_id');
+        });
+};
