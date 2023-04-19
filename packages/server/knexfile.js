e90d060f7fa158d5533774cb5473c98484e4c659
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-04-19 11:36:35 -0500

    Configure SSL for production Postgres auth

--- packages/server/knexfile.js
+++ packages/server/knexfile.js
@@ -37,8 +37,8 @@ module.exports = {
     production: {
         client: 'pg',
         connection: async () => {
+            // Define base connection parameters from POSTGRES_URL and/or standard PG env vars
             const postgresURL = new URL(process.env.POSTGRES_URL);
-            postgresURL.searchParams.set('ssl', 'true');
             const hostname = process.env.PGHOST || postgresURL.hostname;
             const port = process.env.PGPORT || postgresURL.port;
             const username = process.env.PGUSER || postgresURL.username;
@@ -51,36 +51,34 @@ module.exports = {
                 host: hostname,
                 port,
                 database: dbname,
+                ssl: {
+                    ca: fs.readFileSync(path.resolve('./rds-combined-ca-bundle.pem'), "utf-8"),
+                },
             };
 
+            // Attempt to get a signed token for IAM auth or fall back to basic auth
             const tokenExpiration = new Date();
             const signer = new Signer({ hostname, port, username });
             const token = await signer.getAuthToken().then((authToken) => {
                 tokenExpiration.setSeconds(tokenExpiration.getSeconds() + 900);
                 return authToken;
-            }).catch(() => {});
+            }).catch(() => { });
             if (!token) {
                 console.warn('Failed to sign IAM auth token for Postgres, will use basic auth');
                 return basicAuthConfig;
             }
 
             const iamAuthConfig = {
-                user: username,
+                ...basicAuthConfig,
                 password: token,
-                host: hostname,
-                port,
-                database: dbname,
-                ssl: {
-                    ca: fs.readFileSync(path.resolve('./rds-combined-ca-bundle.pem'), "utf-8"),
-                },
-                expirationChecker: () => { return tokenExpiration <= new Date(); },
+                expirationChecker: () => { return tokenExpiration <= new Date(); }
             };
 
             try {
-                // Test IAM authentication:
-                await require('knex')({ client: 'pg', connection: iamAuthConfig}).raw('select 1');
+                // Test IAM authentication
+                await require('knex')({ client: 'pg', connection: iamAuthConfig }).raw('select 1');
             } catch (e) {
-                console.warn('Postgres IAM auth failed, will use POSTGRES_URL');
+                console.warn('Postgres connectivity test with IAM auth failed, will use basic auth');
                 return basicAuthConfig;
             }
             return iamAuthConfig;
