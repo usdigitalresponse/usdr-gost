d8cf39f05e54af4554ca9d7e8a09a3bb05582418
Vi Tran <vi@vismb.local>
2023-05-26 17:21:26 -0600

    feat: add footer from 1401

--- packages/client/src/components/Modals/SearchPanel.vue
+++ packages/client/src/components/Modals/SearchPanel.vue
@@ -11,6 +11,15 @@
         shadow
       >
       <b-form-group></b-form-group>
+      <template #footer="{ hide }">
+       <div class="d-flex text-light align-items-center px-3 py-2 sidebar-footer">
+        <b-button size="sm" @click="hide" variant="outline-primary" class="borderless-button">Close</b-button>
+        <div class="right-button-container">
+          <b-button size="sm" @click="hide" variant="outline-primary">Save New Search</b-button>
+          <b-button size="sm" @click="hide" variant="primary">Apply</b-button>
+        </div>
+       </div>
+      </template>
       </b-sidebar>
     </div>
   </template>
@@ -50,5 +59,17 @@ export default {
 .search-panel > .b-sidebar > .b-sidebar-header{
   font-size: 1.25rem;
   border-bottom: 1.5px solid #e8e8e8;
+}
+.sidebar-footer {
+  border-top: 1.5px solid #e8e8e8;
+  justify-content: space-between;
+}
+.borderless-button {
+  border-color: transparent;
+}
+.right-button-container{
+  width: 190px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
