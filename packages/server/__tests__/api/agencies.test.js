08dc857d9a0fb721885855ad46d02c0e21ee4516
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-05-26 14:28:20 -0500

    Add unit tests for deleting agencies

--- packages/server/__tests__/api/agencies.test.js
+++ packages/server/__tests__/api/agencies.test.js
@@ -3,6 +3,10 @@ const sinon = require('sinon');
 const { getSessionCookie, makeTestServer } = require('./utils');
 const emailService = require('../../src/lib/email/service-email');
 const email = require('../../src/lib/email');
+const {
+    createAgency, createUser, deleteUser, deleteAgency,
+} = require('../../src/db');
+const fixtures = require('../db/seeds/fixtures');
 
 describe('`/api/organizations/:organizationId/agencies` endpoint', () => {
     const agencies = {
@@ -76,6 +80,94 @@ describe('`/api/organizations/:organizationId/agencies` endpoint', () => {
         it('is forbidden for an agency outside this user\'s tenant', async () => {
             const response = await fetchApi('/agencies', agencies.admin.offLimits, fetchOptions.admin);
             expect(response.statusText).to.equal('Forbidden');
+        });
+    });
+
+    context('DELETE /api/organizations/:organizationId/agencies/del/:agency', () => {
+        let admin;
+        let deleteRequest;
+        let agency;
+        before(async () => {
+            admin = await createUser({
+                name: 'Test Admin',
+                email: 'admin@example.com',
+                agency_id: 0,
+                tenant_id: 1,
+                role_id: fixtures.roles.adminRole.id,
+            });
+            agency = await createAgency({
+                name: 'Agency To Delete',
+                abbreviation: 'ATD',
+                code: 'ATD',
+                parent: 0,
+                tenant_id: 1,
+                warning_threshold: 12,
+                danger_threshold: 34,
+            }, admin.id);
+            deleteRequest = async (agencyToDelete) => fetchApi(
+                `/agencies/del/${agencyToDelete.id}`,
+                admin.agency_id,
+                {
+                    headers: {
+                        cookie: await getSessionCookie(admin.id),
+                        'Content-Type': 'application/json',
+                    },
+                    method: 'delete',
+                    body: JSON.stringify({
+                        warningThreshold: 12, dangerThreshold: 34, ...agencyToDelete,
+                    }),
+                },
+            );
+        });
+
+        it('issues 400 Bad Request when agency has users', async () => {
+            const blockingUser = await createUser({
+                name: 'Some One',
+                email: 'staff@example.com',
+                agency_id: agency.id,
+                tenant_id: agency.tenant_id,
+                role_id: fixtures.roles.staffRole.id,
+            });
+            const response = await deleteRequest(agency);
+            expect(response.status).to.equal(400);
+            expect(await response.text()).to.equal('agency has assigned users');
+            // Cleanup
+            await deleteUser(blockingUser.id);
+        });
+
+        it('issues 400 Bad Request when agency has sub-agencies', async () => {
+            const subAgency = await createAgency({
+                name: 'Child of Agency To Delete',
+                abbreviation: 'COATD',
+                code: 'COATD',
+                parent: agency.id,
+                tenant_id: agency.tenant_id,
+                warning_threshold: 12,
+                danger_threshold: 34,
+            }, admin.id);
+            const response = await deleteRequest(agency);
+            expect(response.status).to.equal(400);
+            expect(await response.text()).to.equal('agency has sub-agencies');
+            // Cleanup
+            const {
+                id, parent, name, abbreviation, warning_threshold, danger_threshold,
+            } = subAgency;
+            await deleteAgency(id, parent, name, abbreviation, warning_threshold, danger_threshold);
+        });
+
+        it('issues 200 OK when agency has no users or sub-agencies', async () => {
+            const subAgency = await createAgency({
+                name: 'Another Child of Agency To Delete',
+                abbreviation: 'ACOATD',
+                code: 'ACOATD',
+                parent: agency.id,
+                tenant_id: agency.tenant_id,
+                warning_threshold: 12,
+                danger_threshold: 34,
+            }, admin.id);
+            console.log(JSON.stringify(subAgency));
+            const response = await deleteRequest(subAgency);
+            expect(response.status).to.equal(200);
         });
     });
 
