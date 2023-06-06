c8a8fc0571eae490615c6505eb7f6014bb5c1030
Tom Z <tzinckgraf@gmail.com>
2023-06-05 21:35:36 -0400

    fix: #1386 Ensure that uploads are automatically sorted by latest validated at the top (#1386)
    * added default sorting order to the Uploads page
    
    * added test case for checking the Upload validated at sort dependency

--- packages/client/tests/unit/arpa_reporter/views/Uploads.spec.js
+++ packages/client/tests/unit/arpa_reporter/views/Uploads.spec.js
@@ -26,6 +26,48 @@ describe('Uploads.vue', () => {
     });
     expect(wrapper.text()).to.include('No uploads');
   });
+
+  it('renders with data and defaults to descending sorting', async () => {
+    const store = new Vuex.Store({
+      state: {
+        agencies: [],
+        allUploads: [{
+          agency_code: 'USDR',
+          agency_id: 0,
+          created_at: new Date().toISOString(),
+          created_by: 'test@usdigitalresponse.org',
+          ec_code: '0',
+          filename: 'TEST_UPLOAD.xlsm',
+          id: '00000000-0000-0000-0000-000000000000',
+          notes: null,
+          reporting_period_id: 64,
+          tenant_id: 1,
+          user_id: 1,
+          validated_at: new Date().toISOString(),
+          validated_by: 1,
+        }],
+      },
+      getters: {
+        periodNames: () => ['September, 2020', 'December, 2020'],
+        agencyName: () => () => 'Test Agency',
+      },
+    });
+
+    const wrapper = mount(Uploads, {
+      store,
+      localVue,
+      stubs: ['router-link'],
+    });
+    expect(wrapper.text()).to.include('00000000');
+
+    const validatedCol = wrapper.find('#vgt-table').findAll('th').at(6);
+    // table sorting doesn't happen until the next tick. this can be found in the
+    // good table test cases
+    // https://github.com/xaksis/vue-good-table/blob/master/test/unit/specs/Table.spec.js#L78
+    await wrapper.vm.$nextTick();
+    expect(validatedCol.classes()).to.include('sorting-desc');
+    expect(validatedCol.classes()).to.include('sortable');
+  });
 });
 
 // NOTE: This file was copied from tests/unit/views/Uploads.spec.js (git @ ada8bfdc98) in the arpa-reporter repo on 2022-09-23T20:05:47.735Z
