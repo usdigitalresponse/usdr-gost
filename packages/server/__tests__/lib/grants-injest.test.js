bed579266f04e178a91c389d3a74fa9933794df5
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-06-08 00:46:27 +0000

    Update tests for grants-ingest lib

--- packages/server/__tests__/lib/grants-injest.test.js
+++ packages/server/__tests__/lib/grants-injest.test.js
@@ -1,9 +1,10 @@
+const _ = require('lodash');
 const { expect } = require('chai');
 const sinon = require('sinon');
 const { processMessages, receiveNextMessageBatch } = require('../../src/lib/grants-ingest');
 
-describe('processMessages', () => {
-    const queueUrl = 'https://sqs.region.amazonaws.com/123456789012/grant-ingest-events';
+describe('processMessages', async () => {
+    const queueUrl = 'https://sqs.us-west-2.amazonaws.com/123456789012/grants-ingest-events';
     let knexQuery;
     let knexStub;
     let sqsStub;
@@ -24,6 +25,7 @@ describe('processMessages', () => {
     });
 
     it('should process messages successfully', async () => {
+        const jsonMatcher = (expectedString) => (actualString) => _.isEqual(JSON.parse(expectedString), JSON.parse(actualString));
         const messages = [
             {
                 Body: JSON.stringify({
@@ -46,7 +48,7 @@ describe('processMessages', () => {
             {
                 Body: JSON.stringify({
                     OpportunityId: '2',
-                    OpportunityNumber: 'for-some-reason-not-a-number',
+                    OpportunityNumber: 'nope-no-numbers-here',
                     AgencyCode: 'ZYX-ABC-PZOX',
                     AwardCeiling: '987',
                     AwardFloor: 'unparseable',
@@ -60,12 +62,29 @@ describe('processMessages', () => {
                 }),
                 ReceiptHandle: 'receipt-handle-1',
             },
+            {
+                Body: JSON.stringify({
+                    grant_id: '3',
+                    OpportunityNumber: 'not-a-number-either',
+                    AgencyCode: 'CUVY-MWN-IVUB',
+                    AwardCeiling: 'unparseable',
+                    AwardFloor: '7654',
+                    CostSharingOrMatchingRequirement: false,
+                    OpportunityTitle: 'Superb opportunity',
+                    CFDANumbers: ['98.765', '87.654'],
+                    PostDate: '05062023',
+                    OpportunityCategory: 'M',
+                    Description: 'Here is a description of this superb grant',
+                    EligibleApplicants: ['25', '20', '13', '12', '11', '10'],
+                }),
+                ReceiptHandle: 'receipt-handle-1',
+            },
         ];
 
         await processMessages(knexStub, sqsStub, queueUrl, messages);
 
         sinon.assert.calledWithExactly(knexStub, 'grants');
-        sinon.assert.callCount(knexStub, 2);
+        sinon.assert.callCount(knexStub, messages.length);
         sinon.assert.calledWith(knexQuery.insert, sinon.match({
             status: 'inbox',
             grant_id: '1',
@@ -85,11 +104,12 @@ describe('processMessages', () => {
             description: 'Here is a description of this cool grant',
             eligibility_codes: '00 01 02 03',
             opportunity_status: 'posted',
+            raw_body: sinon.match(jsonMatcher(messages[0].Body)),
         }));
         sinon.assert.calledWith(knexQuery.insert, sinon.match({
             status: 'inbox',
             grant_id: '2',
-            grant_number: 'for-some-reason-not-a-number',
+            grant_number: 'nope-no-numbers-here',
             agency_code: 'ZYX-ABC-PZOX',
             award_ceiling: 987,
             award_floor: undefined,
@@ -105,8 +125,30 @@ describe('processMessages', () => {
             description: 'Here is a description of this awesome grant',
             eligibility_codes: '25 20 13 12 11 10',
             opportunity_status: 'posted',
+            raw_body: sinon.match(jsonMatcher(messages[1].Body)),
         }));
-        sinon.assert.callCount(sqsStub.send, 2);
+        sinon.assert.calledWith(knexQuery.insert, sinon.match({
+            status: 'inbox',
+            grant_id: '3',
+            grant_number: 'not-a-number-either',
+            agency_code: 'CUVY-MWN-IVUB',
+            award_ceiling: undefined,
+            award_floor: 7654,
+            cost_sharing: 'No',
+            title: 'Superb opportunity',
+            cfda_list: '98.765, 87.654',
+            open_date: '2023-05-06',
+            close_date: '2100-01-01',
+            notes: 'auto-inserted by script',
+            search_terms: '[in title/desc]+',
+            reviewer_name: 'none',
+            opportunity_category: 'Mandatory',
+            description: 'Here is a description of this superb grant',
+            eligibility_codes: '25 20 13 12 11 10',
+            opportunity_status: 'posted',
+            raw_body: sinon.match(jsonMatcher(messages[2].Body)),
+        }));
+        sinon.assert.callCount(sqsStub.send, messages.length);
         sinon.assert.calledWith(sqsStub.send, sinon.match({
             input: { QueueUrl: queueUrl, ReceiptHandle: messages[0].ReceiptHandle },
         }));
@@ -118,7 +160,7 @@ describe('processMessages', () => {
     it('should skip processing message when error parsing json', async () => {
         const messages = [
             {
-                Body: JSON.stringify({ OpportunityId: 1 }),
+                Body: JSON.stringify({ OpportunityId: 1, PostDate: '05062023' }),
                 ReceiptHandle: 'receipt-handle-1',
             },
             {
@@ -138,14 +180,40 @@ describe('processMessages', () => {
         }));
     });
 
-    it('should not delete the sqs message when error saving grant', async () => {
+    it('should skip processing message when error parsing date', async () => {
         const messages = [
             {
-                Body: JSON.stringify({ OpportunityId: 1 }),
+                Body: JSON.stringify({ OpportunityId: 1, PostDate: '06072023' }),
                 ReceiptHandle: 'receipt-handle-1',
             },
             {
-                Body: JSON.stringify({ OpportunityId: 2 }),
+                Body: JSON.stringify({
+                    OpportunityId: 2,
+                    PostDate: 'this-date-cannot-be-parsed PM',
+                }),
+                ReceiptHandle: 'receipt-handle-2',
+            },
+        ];
+
+        await processMessages(knexStub, sqsStub, queueUrl, messages);
+
+        sinon.assert.calledWithExactly(knexStub, 'grants');
+        sinon.assert.callCount(knexStub, 1);
+        sinon.assert.calledWith(knexQuery.insert, sinon.match({ grant_id: 1 }));
+        sinon.assert.callCount(sqsStub.send, 1);
+        sinon.assert.calledWith(sqsStub.send, sinon.match({
+            input: { QueueUrl: queueUrl, ReceiptHandle: messages[0].ReceiptHandle },
+        }));
+    });
+
+    it('should not delete the sqs message when error saving grant', async () => {
+        const messages = [
+            {
+                Body: JSON.stringify({ OpportunityId: 1, PostDate: '2023-06-07' }),
+                ReceiptHandle: 'receipt-handle-1',
+            },
+            {
+                Body: JSON.stringify({ OpportunityId: 2, PostDate: '2023-06-07' }),
                 ReceiptHandle: 'receipt-handle-2',
             },
         ];
@@ -167,11 +235,11 @@ describe('processMessages', () => {
     it('should handle error deleting message without interrupting others', async () => {
         const messages = [
             {
-                Body: JSON.stringify({ OpportunityId: 1 }),
+                Body: JSON.stringify({ OpportunityId: 1, PostDate: '2023-06-07' }),
                 ReceiptHandle: 'receipt-handle-1',
             },
             {
-                Body: JSON.stringify({ OpportunityId: 2 }),
+                Body: JSON.stringify({ OpportunityId: 2, PostDate: '2023-06-07' }),
                 ReceiptHandle: 'receipt-handle-2',
             },
         ];
@@ -198,7 +266,7 @@ describe('processMessages', () => {
 });
 
 describe('receiveNextMessageBatch', () => {
-    const queueUrl = 'https://sqs.region.amazonaws.com/123456789012/grant-ingest-events';
+    const queueUrl = 'https://sqs.us-west-2.amazonaws.com/123456789012/grants-ingest-events';
     const receiveCommandArgs = { QueueUrl: queueUrl, WaitTimeSeconds: 20, MaxNumberOfMessages: 10 };
     let sqsStub;
 
