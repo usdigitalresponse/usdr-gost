0187bedac57c3c8d0c7e6a02771b5ddad23d34e0
Tyler Hendrickson <hendrickson.tsh@gmail.com>
2023-06-09 20:20:39 -0500

    938 consume grants from sqs (#1495)
    * Add @aws-sdk/client-sqs to server dependencies
    
    * Add getSQSClient() helper to gost-aws lib
    
    * Migration: add grants.award_floor column
    
    * Create grants-ingest lib module
    
    * Add script to consume grant modification events from SQS
    
    * Init localstack with grant-ingest-events SQS queue
    
    * Add tests for grants-ingest lib
    
    * Set default GRANTS_INGEST_EVENTS_QUEUE_URL env var
    
    * Add datadog tracing to consumeGrantModifications script

---
+++ packages/server/migrations/20230606020935_add-award_floor-column-to-grants.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.table('grants', (table) => {
+        table.string('award_floor');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.table('grants', (table) => {
+        table.dropColumn('award_floor');
+    });
+};
