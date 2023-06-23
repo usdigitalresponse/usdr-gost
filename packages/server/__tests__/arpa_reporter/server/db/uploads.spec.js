9da9f6a72116362e87d45cace49e2fa27d573e37
Tom Z <tzinckgraf@gmail.com>
2023-06-22 22:06:26 -0400

    fix: #1268 the backend piece that adds an invalidated_at and invalidated_by to the uploads table. These can then be excluded by various reporting, such as the audit report.

--- packages/server/__tests__/arpa_reporter/server/db/uploads.spec.js
+++ packages/server/__tests__/arpa_reporter/server/db/uploads.spec.js
@@ -1,5 +1,6 @@
 const uploads = requireSrc(__filename);
 const assert = require('assert');
+const fixtures = require('../fixtures/fixtures');
 const { withTenantId } = require('../helpers/with-tenant-id');
 const { knex } = require('../mocha_init');
 
@@ -40,6 +41,29 @@ describe('db/uploads.js', () => {
                 upload.reporting_period_id = 12345;
                 assert.rejects(async () => withTenantId(TENANT_ID, uploads.createUpload(upload)));
             });
+        });
+    });
+
+    context('usedForTreasuryExport', () => {
+        before(async () => {
+            await fixtures.seed(knex);
+        });
+
+        after(async () => {
+            await fixtures.clean(knex);
+            await knex.destroy(knex);
+        });
+
+        it('should only have two results', async () => {
+            const rows = await withTenantId(fixtures.TENANT_ID, async () => {
+                const r = await uploads.usedForTreasuryExport(fixtures.reportingPeriods.q1_2021.id);
+                return r;
+            });
+            const uploadsFiltered = Object.values(fixtures.uploads)
+                .filter((f) => f.tenant_id === fixtures.TENANT_ID)
+                .filter((f) => f.invalidated_at === null)
+                .filter((f) => f.reporting_period_id === fixtures.reportingPeriods.q1_2021.id);
+            assert.equal(rows.length, uploadsFiltered.length);
         });
     });
 });
---
+++ packages/server/__tests__/arpa_reporter/server/fixtures/fixtures.js
@@ -0,0 +1,110 @@
+const { agencies, users } = require('../../../db/seeds/fixtures');
+
+const TENANT_ID = 0;
+
+const TABLES = {
+    reporting_periods: 'reporting_periods',
+    uploads: 'uploads',
+};
+
+const reportingPeriods = {
+    q1_2021: {
+        id: 1,
+        name: 'Quarterly 1',
+        start_date: '2021-01-01',
+        end_date: '2021-03-31',
+        certified_at: null,
+        certified_by: null,
+        tenant_id: TENANT_ID,
+        template_filename: '',
+    },
+    q2_2021: {
+        id: 2,
+        name: 'Quarterly 2',
+        start_date: '2021-04-01',
+        end_date: '2021-06-30',
+        certified_at: null,
+        certified_by: null,
+        tenant_id: TENANT_ID,
+        template_filename: '',
+    },
+};
+
+const uploads = {
+    upload1: {
+        filename: 'test-filename-1.xlsm',
+        reporting_period_id: reportingPeriods.q1_2021.id,
+        user_id: users.adminUser.id,
+        agency_id: agencies.accountancy.id,
+        validated_at: '2022-01-01',
+        ec_code: '1.1',
+        tenant_id: TENANT_ID,
+        id: '00000000-0000-0000-0000-000000000000',
+        notes: null,
+    },
+    upload2: {
+        filename: 'test-filename-2.xlsm',
+        reporting_period_id: reportingPeriods.q1_2021.id,
+        user_id: users.adminUser.id,
+        agency_id: agencies.accountancy.id,
+        ec_code: '1.1',
+        tenant_id: TENANT_ID,
+        id: '00000000-0000-0000-0000-000000000001',
+        notes: null,
+    },
+    upload3: {
+        filename: 'test-filename-3.xlsm',
+        reporting_period_id: reportingPeriods.q1_2021.id,
+        user_id: users.adminUser.id,
+        agency_id: agencies.accountancy.id,
+        ec_code: '1.1',
+        tenant_id: TENANT_ID + 1,
+        id: '00000000-0000-0000-0000-000000000002',
+        notes: null,
+    },
+    upload4_invalidated: {
+        filename: 'test-filename-4.xlsm',
+        reporting_period_id: reportingPeriods.q1_2021.id,
+        user_id: users.adminUser.id,
+        agency_id: agencies.accountancy.id,
+        invalidated_at: '2023-03-02',
+        invalidated_by: users.staffUser.id,
+        ec_code: '1.1',
+        tenant_id: TENANT_ID,
+        id: '00000000-0000-0000-0000-000000000003',
+        notes: null,
+    },
+    upload5_new_quarter: {
+        filename: 'test-filename-5.xlsm',
+        reporting_period_id: reportingPeriods.q2_2021.id,
+        user_id: users.adminUser.id,
+        agency_id: agencies.accountancy.id,
+        ec_code: '1.1',
+        tenant_id: TENANT_ID,
+        id: '00000000-0000-0000-0000-000000000004',
+        notes: null,
+    },
+};
+
+module.exports = {
+    TABLES,
+    reportingPeriods,
+    uploads,
+    TENANT_ID,
+};
+
+module.exports.clean = async (knex) => {
+    await knex.raw('TRUNCATE TABLE users CASCADE');
+    await knex.raw('TRUNCATE TABLE uploads CASCADE');
+    await knex.raw('TRUNCATE TABLE reporting_periods CASCADE');
+};
+
+module.exports.seed = async (knex) => {
+    await knex.raw('TRUNCATE TABLE users CASCADE');
+    await knex.raw('TRUNCATE TABLE uploads CASCADE');
+    await knex.raw('TRUNCATE TABLE reporting_periods CASCADE');
+
+    await knex('users').insert(Object.values(users));
+    await knex('reporting_periods').insert(Object.values(reportingPeriods));
+    await knex('uploads').insert(Object.values(uploads));
+};
