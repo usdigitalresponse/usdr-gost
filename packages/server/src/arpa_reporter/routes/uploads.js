f66aa2940094e2bb925819017b6aa00be7dbfbf5
Tom Z <tzinckgraf@gmail.com>
2023-07-10 23:28:06 -0400

    fix: #1268 - added more test cases and require admin user

--- packages/server/src/arpa_reporter/routes/uploads.js
+++ packages/server/src/arpa_reporter/routes/uploads.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const router = express.Router();
 const multer = require('multer');
-const { requireUser } = require('../../lib/access-helpers');
+const { requireUser, requireAdminUser } = require('../../lib/access-helpers');
 
 const multerUpload = multer({ storage: multer.memoryStorage() });
 
@@ -155,7 +155,7 @@ router.post('/:id/validate', requireUser, async (req, res) => {
     }
 });
 
-router.post('/:id/invalidate', requireUser, async (req, res) => {
+router.post('/:id/invalidate', requireAdminUser, async (req, res) => {
     const { id } = req.params;
 
     const { user } = req.session;
