8b7fa0391335fd87c0eecf20adb468055d64fdf6
Michelle Lam <mlam8428@gmail.com>
2023-07-25 22:49:50 -0400

    Add column for capital expenditure amount in new project summary sheet

--- packages/server/src/arpa_reporter/lib/audit-report.js
+++ packages/server/src/arpa_reporter/lib/audit-report.js
@@ -128,6 +128,8 @@ async function getProjectSummaryGroupedByProjectRow(data) {
         ].forEach((columnName) => { row[columnName] = 0; });
     });
 
+    row['Capital Expenditure Amount'] = 0;
+
     // set values in each column
     records.forEach(async (r) => {
         const reportingPeriodEndDate = reportingPeriods.filter((reportingPeriod) => r.upload.reporting_period_id === reportingPeriod.id)[0].end_date;
@@ -135,6 +137,7 @@ async function getProjectSummaryGroupedByProjectRow(data) {
         row[`${reportingPeriodEndDate} Total Aggregate Obligations`] += (r.content.Total_Obligations__c || 0);
         row[`${reportingPeriodEndDate} Total Obligations for Awards Greater or Equal to $50k`] += (record.content.Award_Amount__c || 0);
         row[`${reportingPeriodEndDate} Total Expenditures for Awards Greater or Equal to $50k`] += (record.content.Expenditure_Amount__c || 0);
+        row['Capital Expenditure Amount'] += (r.content.Expenditure_Amount__c || 0);
     });
 
     return row;
