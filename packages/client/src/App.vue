81cde13b17e8cc8631fae322bb5e2fc671ad0382
Joey Yang <joey@thirdslant.com>
2023-08-05 13:35:31 -0700

    feat: implement USDR color stacks, semantic tokens, fonts

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -17,7 +17,6 @@ export default {
 @import '../scss/custom.scss';
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
