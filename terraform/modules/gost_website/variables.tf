056c51023a1ea91669585cbe2620f0b12d20adfd
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-08-07 17:56:54 -0500

    Allow module.gost_website.feature_flags to be an object of arbitrary types

--- terraform/modules/gost_website/variables.tf
+++ terraform/modules/gost_website/variables.tf
@@ -37,8 +37,12 @@ variable "gost_api_domain" {
 
 variable "feature_flags" {
   description = "Feature flags for configuring the website runtime"
-  type        = object({})
+  type        = any
   default     = {}
+  validation {
+    condition     = can(lookup(var.feature_flags, uuid(), "default"))
+    error_message = "Value must be an object."
+  }
   validation {
     condition     = can(jsonencode(var.feature_flags))
     error_message = "Value must be JSON-serializable."
