3e6af1b5521b06b8934cc63681ab265b485475fa
tyler <thendrickson@usdigitalresponse.org>
2023-08-08 19:44:04 +0000

    Deprecate window.apiURLForGOST for window.APP_CONFIG.apiURLForGost

--- terraform/modules/gost_website/tpl/deploy-config.js
+++ terraform/modules/gost_website/tpl/deploy-config.js
@@ -1,3 +1,4 @@
 window.APP_CONFIG = window.APP_CONFIG || {};
-window.apiURLForGOST = 'https://${gost_api_domain}/';
+window.APP_CONFIG.apiURLForGOST = 'https://${gost_api_domain}/';
+window.apiURLForGOST = window.APP_CONFIG.apiURLForGOST; // Legacy
 window.APP_CONFIG.featureFlags = ${feature_flags};
