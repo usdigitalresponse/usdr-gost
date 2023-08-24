fdbc52e1eba4f20958608611afd1131bc3a42d02
Jake Kreider <624510+jakekreider@users.noreply.github.com>
2023-08-24 15:02:37 -0500

    Fixes for #1797 (#1813)
    - Don't change URL/route on edit and clear buttons
    - Ensure table refreshes when saving changes to a searcch

--- packages/client/src/components/Modals/SearchPanel.vue
+++ packages/client/src/components/Modals/SearchPanel.vue
@@ -246,7 +246,7 @@ export default {
     async saveSearch() {
       this.apply();
       let searchId;
-      if (this.formData.searchId !== undefined && this.formData.searchId !== null) {
+      if (this.isEditMode) {
         this.updateSavedSearch({
           searchId: this.formData.searchId,
           searchInfo: {
@@ -255,6 +255,7 @@ export default {
           },
         });
         searchId = this.formData.searchId;
+        this.$emit('filters-applied');
       } else {
         const res = await this.createSavedSearch({
           searchInfo: {
