2b46c89fac2ec0b3340cb4a11e6c19ea40d5b6b1
Aditya Sridhar <aditus06@gmail.com>
2023-09-12 17:01:18 -0400

    fix: opp-status indexing

---
+++ packages/server/migrations/20230912195046_add-grants-opportunity-status-index.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.raw('CREATE INDEX IF NOT EXISTS idx_grants_opportunity_status ON grants USING hash (opportunity_status);');
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.raw('DROP INDEX IF EXISTS idx_grants_opportunity_status;');
+};
