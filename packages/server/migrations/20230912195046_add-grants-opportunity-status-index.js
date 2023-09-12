adac685c8300abd93cfa97b853814e871133691d
Aditya Sridhar <aditus06@gmail.com>
2023-09-12 19:45:44 -0400

    fix: changes to btree partial index

--- packages/server/migrations/20230912195046_add-grants-opportunity-status-index.js
+++ packages/server/migrations/20230912195046_add-grants-opportunity-status-index.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-    return knex.raw('CREATE INDEX IF NOT EXISTS idx_grants_opportunity_status ON grants USING hash (opportunity_status);');
+    return knex.raw(`CREATE INDEX IF NOT EXISTS idx_grants_opportunity_status ON grants USING btree (opportunity_status) WHERE (opportunity_status <> 'archived'::text)`);
 };
 
 /**
