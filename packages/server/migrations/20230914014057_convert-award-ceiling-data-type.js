ab5e0af32255d990f6b77e10b74c825f813586e2
Aditya Sridhar <aditus06@gmail.com>
2023-09-14 16:07:34 -0400

    fix: adds award-floor to bigint

--- packages/server/migrations/20230914014057_convert-award-ceiling-data-type.js
+++ packages/server/migrations/20230914014057_convert-award-ceiling-data-type.js
@@ -3,7 +3,10 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-    return knex.raw(`ALTER TABLE grants ALTER COLUMN award_ceiling TYPE bigint USING award_ceiling::bigint`);
+    return knex.raw(`
+        ALTER TABLE grants
+        ALTER COLUMN award_ceiling TYPE bigint USING award_ceiling::bigint,
+        ALTER COLUMN award_floor TYPE bigint USING award_floor::bigint`);
 };
 
 /**
@@ -11,5 +14,8 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-    return knex.raw(`ALTER TABLE grants ALTER COLUMN award_ceiling TYPE text USING award_ceiling::text`);
+    return knex.raw(`
+    ALTER TABLE grants
+    ALTER COLUMN award_ceiling TYPE text USING award_ceiling::text,
+    ALTER COLUMN award_floor TYPE text USING award_floor::text`);
 };
