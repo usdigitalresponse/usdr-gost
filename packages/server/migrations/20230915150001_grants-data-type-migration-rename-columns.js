f5cd1bafae6754905523814b9ce102ac7a297947
Aditya Sridhar <aditus06@gmail.com>
2023-09-15 14:58:23 -0400

    fix: alter table run as multiple queries

--- packages/server/migrations/20230915150001_grants-data-type-migration-rename-columns.js
+++ packages/server/migrations/20230915150001_grants-data-type-migration-rename-columns.js
@@ -3,17 +3,19 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-    return knex.raw(`
-        ALTER TABLE grants
-            RENAME COLUMN award_ceiling TO award_ceiling_old,
-            RENAME COLUMN award_floor TO award_floor_old,
-            RENAME COLUMN open_date TO open_date_old,
-            RENAME COLUMN close_date TO close_date_old,
-            RENAME COLUMN award_ceiling_temp TO award_ceiling,
-            RENAME COLUMN award_floor_temp TO award_floor,
-            RENAME COLUMN open_date_temp TO open_date,
-            RENAME COLUMN close_date_temp TO close_date
-    `);
+    const queries = [
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_ceiling TO award_ceiling_old`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_floor TO award_floor_old`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN open_date TO open_date_old`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN close_date TO close_date_old`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_ceiling_temp TO award_ceiling`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_floor_temp TO award_floor`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN open_date_temp TO open_date`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN close_date_temp TO close_date`),
+    ];
+    console.log(queries.join(';'));
+    const multiQuery = queries.join(';');
+    return knex.raw(multiQuery);
 };
 
 /**
@@ -21,15 +23,17 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-    return knex.raw(`
-        ALTER TABLE grants
-            RENAME COLUMN award_ceiling TO award_ceiling_temp,
-            RENAME COLUMN award_floor TO award_floor_temp,
-            REANME COLUMN open_date TO open_date_temp,
-            RENAME COLUMN close_date TO close_date_temp,
-            RENAME COLUMN award_ceiling_old TO award_ceiling,
-            RENAME COLUMN award_floor_old TO award_floor,
-            RENAME COLUMN open_date_old TO open_date,
-            REANME COLUMN close_date_old TO close_date
-    `);
+    const queries = [
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_ceiling TO award_ceiling_temp`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_floor TO award_floor_temp`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN open_date TO open_date_temp`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN close_date TO close_date_temp`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_ceiling_old TO award_ceiling`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN award_floor_old TO award_floor`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN open_date_old TO open_date`),
+        knex.raw(`ALTER TABLE grants RENAME COLUMN close_date_old TO close_date`),
+    ];
+    console.log(queries.join(';'));
+    const multiQuery = queries.join(';');
+    return knex.raw(multiQuery);
 };
