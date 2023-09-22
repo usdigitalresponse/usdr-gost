6fd40ffb9c744d14dfb60e82b0ff2a1947ceecdf
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-09-22 21:44:28 +0000

    Add script to drive ARPA audit reports from SQS

---
+++ packages/server/src/scripts/arpaAuditReport.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+const tracer = require('dd-trace').init(); // eslint-disable-line no-unused-vars
+const { ReceiveMessageCommand, DeleteMessageCommand } = require('@aws-sdk/client-sqs');
+const { getSQSClient } = require('../lib/gost-aws');
+const { processSQSMessageRequest } = require('../arpa_reporter/lib/audit-report');
+
+// eslint-disable-block no-await-in-loop
+async function main() {
+    let shutDownRequested = false;
+    const requestShutdown = (signal) => {
+        console.log(`${signal} received. Requesting shutdown...`);
+        shutDownRequested = true;
+    };
+    process.on('SIGTERM', requestShutdown);
+    process.on('SIGINT', requestShutdown);
+
+    const queueUrl = process.env.TASK_QUEUE_URL;
+    const sqs = getSQSClient();
+    while (shutDownRequested === false) {
+        console.log(`Long-polling next SQS message batch from ${queueUrl}`);
+        // eslint-disable-next-line no-await-in-loop
+        const receiveResp = await sqs.send(new ReceiveMessageCommand({
+            QueueUrl: queueUrl,
+            WaitTimeSeconds: 20,
+            MaxNumberOfMessages: 1,
+        }));
+        const message = (receiveResp?.Messages || [])[0];
+        if (message !== undefined) {
+            // eslint-disable-next-line no-await-in-loop
+            const processingSuccessful = await processSQSMessageRequest(message);
+            if (processingSuccessful === true) {
+                // eslint-disable-next-line no-await-in-loop
+                await sqs.send(new DeleteMessageCommand({
+                    QueueUrl: queueUrl,
+                    ReceiptHandle: message.ReceiptHandle,
+                }));
+            }
+        } else {
+            console.log('Empty messages batch received from SQS');
+        }
+    }
+    console.log('Shutting down');
+}
+
+if (require.main === module) {
+    main().then(() => process.exit());
+}
