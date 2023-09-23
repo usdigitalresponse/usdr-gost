d45ed9a9645ae653a11cb60e2b76f4bb281c5d34
Thelma Boamah <thelma.a.boamah@gmail.com>
2023-09-23 01:04:06 -0400

    adds static content and styling

--- packages/client/src/views/MyProfile.vue
+++ packages/client/src/views/MyProfile.vue
@@ -1,5 +1,21 @@
 <template>
   <section style="max-width: 516px;" class="mx-auto my-5 px-2">
     <h2>My Profile</h2>
+    <b-row style="margin-top: 4rem;">
+      <b-col>
+        <b-avatar text="CW" size="5rem"></b-avatar>
+      </b-col>
+      <b-col cols="7">
+        <p class="mb-2 h6"><b>Caitlin Winner</b></p>
+        <p class="mb-2">caitlin.winner@usdr.org</p>
+        <p class="mb-2">City of Sparks</p>
+      </b-col>
+      <b-col class="text-end">
+        <b-button variant="primary" size="md">
+          <b-icon icon="pencil-fill" scale="0.8"></b-icon>
+          <span class="ml-1">Edit</span>
+        </b-button>
+      </b-col>
+    </b-row>
   </section>
 </template>
