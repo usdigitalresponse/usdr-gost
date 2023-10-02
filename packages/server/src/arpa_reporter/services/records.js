b42e70bdff462e223a3bfe9afee855b66738452f
tyler <hendrickson.tsh@gmail.com>
2023-10-02 19:44:23 +0000

    Explicitly check null/undefined in logs

--- packages/server/src/arpa_reporter/services/records.js
+++ packages/server/src/arpa_reporter/services/records.js
@@ -181,6 +181,10 @@ async function recordsForUpload(upload, req = useRequest()) {
         log(`initializing req.recordsForUpload cache`);
         req.recordsForUpload = {};
     }
+    console.log(`Check: (req === undefined) is ${req === undefined}`);
+    console.log(`Check: (req === null) is ${req === null}`);
+    console.log(`Check: (req.recordsForUpload === null) is ${req.recordsForUpload === null}`);
+    console.log(`Check: (req.recordsForUpload === undefined) is ${req.recordsForUpload === undefined}`);
     if (req.recordsForUpload[upload.id]) {
         log(`recordsForUpload(${upload.id}): reading from cache`);
         return req.recordsForUpload[upload.id];
