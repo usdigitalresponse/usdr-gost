b9321121b8ae2539f063a6810eb9e25fa058afc4
Aditya Sridhar <aditus06@gmail.com>
2024-01-04 23:02:17 -0500

    fix: adds tests and moves function out

--- packages/server/__tests__/lib/access-helpers.test.js
+++ packages/server/__tests__/lib/access-helpers.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
-const { getAdminAuthInfo } = require('../../src/lib/access-helpers');
+const sinon = require('sinon');
+const { getAdminAuthInfo, isMicrosoftSafeLinksRequest } = require('../../src/lib/access-helpers');
 const db = require('../../src/db');
 const fixtures = require('../db/seeds/fixtures');
 
@@ -86,6 +87,34 @@ describe('Acces Helper Module', () => {
             expect(result.user.id).to.equal(fixtures.users.adminUser.id);
             expect(Object.keys(result)[1]).to.equal('selectedAgency');
             expect(result.selectedAgency).to.equal(fixtures.agencies.subAccountancy.id);
+        });
+    });
+    context('isMicrosoftSafeLinksRequest', () => {
+        it('early-returns if request is from Microsoft Safe Links', async () => {
+            const resFake = sinon.fake.returns(true);
+            const nextFake = sinon.fake.returns(true);
+            const requestFake = {
+                headers: {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47 OneOutlook/1.2023.927.100',
+                    'x-native-host': 'OneOutlook/1.2023.927.100',
+                },
+            };
+            await isMicrosoftSafeLinksRequest(requestFake, resFake, nextFake);
+            expect(resFake.json.calledOnceWith({ message: 'Success' })).to.equal(true);
+            expect(nextFake.notCalled).to.equal(true);
+        });
+        it('proceeds normally if request is not from Microsoft Safe Links', async () => {
+            const resFake = sinon.fake.returns(true);
+            const nextFake = sinon.fake.returns(true);
+            const requestFake = {
+                headers: {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47 NotOutlook/1.2023.927.100',
+                    'x-native-host': 'NotOutlook/1.2023.927.100',
+                },
+            };
+            await isMicrosoftSafeLinksRequest(requestFake, resFake, nextFake);
+            expect(resFake.json.notCalled).to.equal(true);
+            expect(nextFake.calledOnce).to.equal(true);
         });
     });
 });
