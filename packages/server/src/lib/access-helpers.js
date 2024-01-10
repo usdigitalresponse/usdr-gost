827f952dd7f6b56fb25f14ff3ffb6e4e61b7c317
aditya <asridhar@usdigitalresponse.org>
2024-01-10 17:14:23 -0500

    chore: add logging around safe links requests (#2442)
    * chore: add logging around safe links requests
    
    * fix: narrow scope of header data being logged

--- packages/server/src/lib/access-helpers.js
+++ packages/server/src/lib/access-helpers.js
@@ -1,4 +1,5 @@
 const { getUser, inTenant } = require('../db');
+const { log } = require('./logging');
 
 const USDR_TENANT_ID = 1;
 const USDR_AGENCY_ID = 0;
@@ -128,6 +129,11 @@ async function isMicrosoftSafeLinksRequest(req, res, next) {
     const userAgent = req.headers['user-agent'] || '';
     const nativeHost = req.headers['x-native-host'] || '';
     if (userAgent.toLowerCase().includes('oneoutlook') || nativeHost.toLowerCase().includes('oneoutlook')) {
+        log.info({
+            'user-agent': userAgent,
+            'native-host': nativeHost,
+            headers: Object.keys(req.headers),
+        }, 'Microsoft Safe Links request');
         res.json({ message: 'Success' });
         return;
     }
