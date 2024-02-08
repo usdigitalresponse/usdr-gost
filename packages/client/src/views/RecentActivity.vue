bc8c9aa4ccd6b4db3f453808cff17b69e1848756
adele-usdr <146878468+adele-usdr@users.noreply.github.com>
2024-02-07 18:50:42 -0800

    adding spaces

--- packages/client/src/views/RecentActivity.vue
+++ packages/client/src/views/RecentActivity.vue
@@ -200,7 +200,7 @@ export default {
       if (!newGrantsDetailPageEnabled()) {
         return;
       }
-      this.$router.push({ name: 'grantDetail', params: {id: item.grant_id, backButton: true }});
+      this.$router.push({ name: 'grantDetail', params: { id: item.grant_id, backButton: true }});
     },
     exportCSV() {
       this.exportCSVRecentActivities();
