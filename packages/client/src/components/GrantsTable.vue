ef8e81be14db27d25963245fe7d6e27c724625e4
Jeff Mohan <jeffsmohan@gmail.com>
2024-02-26 10:37:03 -0800

    Fix linking from "my grants" to grant details pages

--- packages/client/src/components/GrantsTable.vue
+++ packages/client/src/components/GrantsTable.vue
@@ -439,7 +439,7 @@ export default {
       if (!newGrantsDetailPageEnabled()) {
         return;
       }
-      this.$router.push(`grant/${item.grant_id}`);
+      this.$router.push({name: 'grantDetail', params: {id: item.grant_id}});
       datadogRum.addAction('view grant details', { grant: item });
     },
     onRowSelected(items) {
