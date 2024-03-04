7d6d69a8c2f6811a2f039e0dc12d34ff8c6c0407
Iñaki <iiaranzadi@gmail.com>
2024-03-04 16:27:30 -0600

    [issue-2369] Back or Browse Grants Link from Grant Details

--- packages/client/src/components/GrantsTable.vue
+++ packages/client/src/components/GrantsTable.vue
@@ -440,7 +440,7 @@ export default {
       if (!newGrantsDetailPageEnabled()) {
         return;
       }
-      this.$router.push({ name: 'grantDetail', params: { id: item.grant_id } });
+      this.$router.push({ name: 'grantDetail', params: { id: item.grant_id }, query: { fromApp: true } });
       datadogRum.addAction('view grant details', { grant: item });
     },
     onRowSelected(items) {
