be99d94e2e0dc1f2757497adf3602972f70d86f9
Shelley Nason <sanason@gmail.com>
2024-03-11 18:19:22 -0500

    Drop the column grants.raw_body. (#2750)
    Co-authored-by: Tyler Hendrickson <hendrickson.tsh@gmail.com>

---
+++ packages/server/migrations/20240308203616_drop_grants_raw_body_column.js
@@ -0,0 +1,24 @@
+/**
+ * The text column `raw_body` was replaced with a jsonb column `raw_body_json` in a previous migration.
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.table('grants', (table) => {
+        table.dropColumn('raw_body');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.table('grants', (table) => {
+        table.text('raw_body');
+    }).raw(`
+        UPDATE grants 
+          SET raw_body = raw_body_json::text 
+        WHERE raw_body_json IS NOT NULL;
+    `);
+};
