4dd6ba2c7b967adaa7d3ea3f1156df672434ae2c
Iñaki Aranzadi <iiaranzadi@gmail.com>
2024-03-12 12:25:06 -0500

    Change back link text from `Back to Results` to `Back` (#2736)
    * Change back link text
    
    * Fix margins and change the title style to align to the top
    
    * Fix linting
    
    * Change text to back

--- packages/client/src/components/SearchFilter.vue
+++ packages/client/src/components/SearchFilter.vue
@@ -72,7 +72,7 @@ export default {
 
 </script>
 <style lang="scss">
-@import '../../scss/colors-semantic-tokens.scss';
+@import '@/scss/colors-semantic-tokens.scss';
 
 .inactiveLink {
   pointer-events: none;
