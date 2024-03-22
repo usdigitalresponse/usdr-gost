ef049737472775662d444fbc3ae2d5edafddb195
Jeff Mohan <jeffsmohan@gmail.com>
2024-03-22 10:22:25 -0700

    Fix auto-fixable lint rules

--- packages/client/src/arpa_reporter/App.vue
+++ packages/client/src/arpa_reporter/App.vue
@@ -4,12 +4,6 @@
   </div>
 </template>
 
-<style lang="scss">
-@import 'bootstrap/scss/bootstrap.scss';
-@import 'bootstrap-vue/src/index.scss';
-@import 'vue-select/src/scss/vue-select.scss';
-</style>
-
 <script>
 import '../../arpa_reporter/scss/index.scss';
 import 'bootstrap/dist/js/bootstrap.min';
@@ -23,5 +17,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap.scss';
+@import 'bootstrap-vue/src/index.scss';
+@import 'vue-select/src/scss/vue-select.scss';
+</style>
 
 <!-- NOTE: This file was copied from src/App.vue (git @ ada8bfdc98) in the arpa-reporter repo on 2022-09-23T20:05:47.735Z -->
