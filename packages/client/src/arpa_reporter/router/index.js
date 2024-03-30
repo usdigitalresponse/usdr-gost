1a49ae1e2d0960cbc40ed60f79d003cb562c4d46
Jeff Mohan <126250+jeffsmohan@users.noreply.github.com>
2024-03-30 14:32:36 -0700

    [Cleanup] Adopt eslint vue/recommended: ARPA Reporter: subrecipient views (#2868)
    * Remove files from eslint ignore list
    
    * Fix auto-fixable lint rules
    
    * Rename subrecipient views with `View` postfix

--- packages/client/src/arpa_reporter/router/index.js
+++ packages/client/src/arpa_reporter/router/index.js
@@ -5,6 +5,8 @@ import AgenciesView from '@/arpa_reporter/views/AgenciesView.vue';
 import AgencyView from '@/arpa_reporter/views/AgencyView.vue';
 import HomeView from '@/arpa_reporter/views/HomeView.vue';
 import LoginView from '@/arpa_reporter/views/LoginView.vue';
+import SubrecipientView from '@/arpa_reporter/views/SubrecipientView.vue';
+import SubrecipientsView from '@/arpa_reporter/views/SubrecipientsView.vue';
 import ValidationView from '@/arpa_reporter/views/ValidationView.vue';
 import NewTemplate from '../views/NewTemplate.vue';
 import NewUpload from '../views/NewUpload.vue';
@@ -12,8 +14,6 @@ import Upload from '../views/Upload.vue';
 import Uploads from '../views/Uploads.vue';
 import ReportingPeriod from '../views/ReportingPeriod.vue';
 import ReportingPeriods from '../views/ReportingPeriods.vue';
-import Subrecipient from '../views/Subrecipient.vue';
-import Subrecipients from '../views/Subrecipients.vue';
 import User from '../views/User.vue';
 import Users from '../views/Users.vue';
 import store from '../store/index';
@@ -79,13 +79,13 @@ const routes = [
   {
     path: '/subrecipients',
     name: 'Subrecipients',
-    component: Subrecipients,
+    component: SubrecipientsView,
     meta: { requiresLogin: true },
   },
   {
     path: '/subrecipients/:id',
     name: 'Subrecipient',
-    component: Subrecipient,
+    component: SubrecipientView,
     meta: { requiresLogin: true },
   },
   {
