acf792c33778a4470089e0635d439fa72bf072db
Aditya Sridhar <aditus06@gmail.com>
2024-04-09 17:40:36 -0400

    chore: update all hashicorp/aws versions from 4.67 to 5.44

--- terraform/modules/gost_api/main.tf
+++ terraform/modules/gost_api/main.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.67.0"
+      version = "~> 5.44.0"
     }
     random = {
       source  = "hashicorp/random"
--- terraform/modules/gost_postgres/main.tf
+++ terraform/modules/gost_postgres/main.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.67.0"
+      version = "~> 5.44.0"
     }
     random = {
       source  = "hashicorp/random"
