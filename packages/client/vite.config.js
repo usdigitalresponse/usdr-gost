c862f07738ad6879a0f629690a816810a11919e5
Jeff Mohan <jeffsmohan@gmail.com>
2024-04-15 11:21:55 -0700

    Baseline vite config/setup

---
+++ packages/client/vite.config.js
@@ -0,0 +1,13 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import { createVuePlugin as vue } from "vite-plugin-vue2"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
