0d466e54a3d9f42704102bd687f63bea57f21af5
Shelley Nason <sanason@gmail.com>
2024-04-18 13:25:33 -0500

    Clean up myProfileEnabled flag.

--- packages/client/src/helpers/featureFlags/index.js
+++ packages/client/src/helpers/featureFlags/index.js
@@ -1,13 +1,5 @@
 import { getFeatureFlags } from './utils';
 
-/**
- * @returns { boolean } true if the new grants table should be active, else false.
- */
-
-export function myProfileEnabled() {
-  return getFeatureFlags().myProfileEnabled === true;
-}
-
 export function newTerminologyEnabled() {
   return getFeatureFlags().newTerminologyEnabled === true;
 }
