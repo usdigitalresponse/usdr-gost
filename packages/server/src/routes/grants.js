4722e99e2ad225e909e45b752285877e7143ced3
Jeff Mohan <jeffsmohan@gmail.com>
2024-05-30 09:26:45 -0700

    Remove now-unused PUT endpoint

--- packages/server/src/routes/grants.js
+++ packages/server/src/routes/grants.js
@@ -370,28 +370,6 @@ router.post('/:grantId/assign/agencies', requireUser, async (req, res) => {
     res.json({});
 });
 
-router.put('/:grantId/assign/agencies', requireUser, async (req, res) => {
-    const { user } = req.session;
-    const { grantId } = req.params;
-    const { agencyIds } = req.body;
-
-    const inSameTenant = await db.inTenant(user.tenant_id, agencyIds);
-    if (!inSameTenant) {
-        res.sendStatus(403);
-        return;
-    }
-
-    await db.assignGrantsToAgencies({ grantId, agencyIds, userId: user.id });
-    try {
-        await email.sendGrantAssignedEmails({ grantId, agencyIds, userId: user.id });
-    } catch {
-        res.sendStatus(500);
-        return;
-    }
-
-    res.json({});
-});
-
 router.delete('/:grantId/assign/agencies', requireUser, async (req, res) => {
     const { user } = req.session;
     const { grantId } = req.params;
