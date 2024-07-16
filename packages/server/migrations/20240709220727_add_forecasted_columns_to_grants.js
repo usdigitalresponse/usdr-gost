beed9318ec3ee62256ef2b0df40d1f113f383ed7
mari <2312138+masimons@users.noreply.github.com>
2024-07-16 12:51:42 -0300

    lint

--- packages/server/migrations/20240709220727_add_forecasted_columns_to_grants.js
+++ packages/server/migrations/20240709220727_add_forecasted_columns_to_grants.js
@@ -2,34 +2,34 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.alterTable('grants', (table) => {
-    table.date('estimated_award_date');
-    table.date('estimated_project_start_date');
-    table.date('estimated_synopsis_close_date');
-    table.date('estimated_synopsis_post_date');
-    table.text('estimated_synopsis_close_date_explanation');
-    table.text('fiscal_year');
-    table.text('grantor_contact_name');
-    table.text('grantor_contact_phone_number');
-    table.boolean('is_forecasted').notNullable().defaultTo(false);
-  });
+exports.up = function (knex) {
+    return knex.schema.alterTable('grants', (table) => {
+        table.date('estimated_award_date');
+        table.date('estimated_project_start_date');
+        table.date('estimated_synopsis_close_date');
+        table.date('estimated_synopsis_post_date');
+        table.text('estimated_synopsis_close_date_explanation');
+        table.text('fiscal_year');
+        table.text('grantor_contact_name');
+        table.text('grantor_contact_phone_number');
+        table.boolean('is_forecasted').notNullable().defaultTo(false);
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  return knex.schema.table('grants', (table) => {
-    table.dropColumn('estimated_award_date');
-    table.dropColumn('estimated_project_start_date');
-    table.dropColumn('estimated_synopsis_close_date');
-    table.dropColumn('estimated_synopsis_post_date');
-    table.dropColumn('estimated_synopsis_close_date_explanation');
-    table.dropColumn('fiscal_year');
-    table.dropColumn('grantor_contact_name');
-    table.dropColumn('grantor_contact_phone_number');
-    table.dropColumn('is_forecasted');
-  });
+exports.down = function (knex) {
+    return knex.schema.table('grants', (table) => {
+        table.dropColumn('estimated_award_date');
+        table.dropColumn('estimated_project_start_date');
+        table.dropColumn('estimated_synopsis_close_date');
+        table.dropColumn('estimated_synopsis_post_date');
+        table.dropColumn('estimated_synopsis_close_date_explanation');
+        table.dropColumn('fiscal_year');
+        table.dropColumn('grantor_contact_name');
+        table.dropColumn('grantor_contact_phone_number');
+        table.dropColumn('is_forecasted');
+    });
 };
