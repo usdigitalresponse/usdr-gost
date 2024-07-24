bd0bc94d60e36741ba52b1a753f6bfaf246c9831
mari <2312138+masimons@users.noreply.github.com>
2024-07-24 15:42:03 -0300

    poc

--- packages/server/src/lib/grants-ingest.js
+++ packages/server/src/lib/grants-ingest.js
@@ -58,6 +58,13 @@ function mapSourceDataToGrant(source) {
         grant.opportunity_status = 'archived';
     } else if (today.isSameOrAfter(moment(grant.close_date), 'date')) {
         grant.opportunity_status = 'closed';
+    } else if (today.isBefore(moment(grant.open_date), 'date')) {
+        // depending on how we model the estimated open date, we may need to do the 
+        // date comparison with the new field for estimated date
+        grant.opportunity_status = 'forecasted'
+        // Note: in the knex query, we are deriving the status based on 
+        // archive_date and close_date. Probably we just want to pass
+        // the opportunity_status to the frontend, and not re-derive it here?
     } else {
         grant.opportunity_status = 'posted';
     }
