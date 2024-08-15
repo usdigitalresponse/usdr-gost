d9d0c429eacd82e3ae398de88ea1bb8c269048c9
greg-adams <greg@codescience.com>
2024-08-15 09:33:24 -0700

    add tests

--- packages/server/__tests__/api/grants.test.js
+++ packages/server/__tests__/api/grants.test.js
@@ -4,6 +4,8 @@ const { getSessionCookie, makeTestServer, knex } = require('./utils');
 const { TABLES } = require('../../src/db/constants');
 const db = require('../../src/db');
 const email = require('../../src/lib/email');
+const users = require('../../seeds/dev/ref/users');
+const { seed } = require('../../seeds/dev/01_main');
 
 /*
     In general, these tests ...
@@ -19,6 +21,9 @@ describe('`/api/grants` endpoint', () => {
         offLimits: 0,
         dallasAdmin: 386,
     };
+
+    const adminUser = users.find((usr) => usr.email === 'admin1@nv.example.com');
+    const staffUser = users.find((usr) => usr.email === 'user1@nv.example.com');
 
     const fetchOptions = {
         admin: {
@@ -52,13 +57,15 @@ describe('`/api/grants` endpoint', () => {
         testServer = await makeTestServer();
         fetchApi = testServer.fetchApi;
     });
+
     after(() => {
         testServer.stop();
     });
 
     const sandbox = sinon.createSandbox();
-    afterEach(() => {
+    afterEach(async () => {
         sandbox.restore();
+        await seed(knex);
     });
 
     context('PUT api/grants/:grantId/view/:agencyId', () => {
@@ -616,7 +623,6 @@ describe('`/api/grants` endpoint', () => {
             }
             const extraRowCount = Object.keys(rowsHash).length;
             if (extraRowCount > 0) {
-                console.log(JSON.stringify(rowsHash, null, 2));
                 expect(extraRowCount).to.equal(0);
             }
         });
@@ -724,7 +730,6 @@ HHS-2021-IHS-TPI-0001,Community Health Aide Program:  Tribal Planning &`;
             }
             const extraRowCount = Object.keys(rowsHash).length;
             if (extraRowCount > 0) {
-                console.log(JSON.stringify(rowsHash, null, 2));
                 expect(extraRowCount).to.equal(0);
             }
         });
@@ -871,6 +876,55 @@ HHS-2021-IHS-TPI-0001,Community Health Aide Program:  Tribal Planning &`;
                 const response = await fetchApi(`/grants/next?pagination[currentPage]=1&pagination[perPage]=50&ordering[orderBy]=interested_agencies&criteria[opportunityStatuses]=posted`, agencies.own, fetchOptions.staff);
                 expect(response.status).to.equal(400);
             });
+        });
+    });
+
+    context('GET api/grants/:grantId/notes', () => {
+        const GRANT_ID = '335255';
+
+        let notes;
+        beforeEach(async () => {
+            notes = await knex('grant_notes')
+                .returning('id')
+                .insert([
+                    { grant_id: GRANT_ID, user_id: adminUser.id },
+                    { grant_id: GRANT_ID, user_id: staffUser.id },
+                ]);
+
+            await knex('grant_notes_revisions')
+                .insert({ grant_note_id: notes[0].id, text: 'Test note 1.' });
+
+            await knex('grant_notes_revisions')
+                .insert({ grant_note_id: notes[1].id, text: 'Test note 2.' });
+        });
+
+        it('returns ALL notes for a given grant in DESC order', async () => {
+            const resp = await fetchApi(`/grants/${GRANT_ID}/notes`, agencies.own, fetchOptions.staff);
+            const respBody = await resp.json();
+
+            expect(respBody.notes.length).to.equal(2);
+            expect(respBody.notes[0].text).to.equal('Test note 2.');
+        });
+
+        it('returns notes with LIMIT', async () => {
+            const resp = await fetchApi(`/grants/${GRANT_ID}/notes?limit=1`, agencies.own, fetchOptions.staff);
+            const respBody = await resp.json();
+
+            expect(respBody.notes.length).to.equal(1);
+        });
+
+        it('returns 400 for invalid LIMIT', async () => {
+            const resp = await fetchApi(`/grants/${GRANT_ID}/notes?limit=500`, agencies.own, fetchOptions.staff);
+
+            expect(resp.status).to.equal(400);
+        });
+
+        it('returns notes with PAGINATION', async () => {
+            const resp = await fetchApi(`/grants/${GRANT_ID}/notes?paginateFrom=${notes[0].id}`, agencies.own, fetchOptions.staff);
+            const respBody = await resp.json();
+
+            expect(respBody.notes.length).to.equal(1);
+            expect(respBody.notes[0].text).to.equal('Test note 2.');
         });
     });
 });
