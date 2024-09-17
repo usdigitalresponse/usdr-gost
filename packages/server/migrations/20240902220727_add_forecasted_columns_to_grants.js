558a80e785b022383b0d2ef8ecad231c5da89314
mari <2312138+masimons@users.noreply.github.com>
2024-09-17 12:11:01 -0300

    remove estimated_open_date

--- packages/server/migrations/20240902220727_add_forecasted_columns_to_grants.js
+++ packages/server/migrations/20240902220727_add_forecasted_columns_to_grants.js
@@ -6,7 +6,6 @@ exports.up = function (knex) {
     return knex.schema.alterTable('grants', (table) => {
         table.date('award_date');
         table.date('forecast_creation_date');
-        table.date('estimated_open_date');
         table.date('fiscal_year');
         table.text('grantor_contact_name');
         table.text('grantor_contact_phone_number');
@@ -21,7 +20,6 @@ exports.down = function (knex) {
     return knex.schema.table('grants', (table) => {
         table.dropColumn('award_date');
         table.dropColumn('forecast_creation_date');
-        table.dropColumn('estimated_open_date');
         table.dropColumn('fiscal_year');
         table.dropColumn('grantor_contact_name');
         table.dropColumn('grantor_contact_phone_number');
