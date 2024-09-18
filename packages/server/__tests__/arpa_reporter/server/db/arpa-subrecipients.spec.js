7bec3d1d312535e3ff0fcf1780d3b20d68dbf13d
Aditya Sridhar <aditus06@gmail.com>
2024-09-18 17:19:31 -0400

    feat: add tests for finding subrecipients

---
+++ packages/server/__tests__/arpa_reporter/server/db/arpa-subrecipients.spec.js
@@ -0,0 +1,50 @@
+/*
+--------------------------------------------------------------------------------
+-                     tests/db/arpa-subrecipients.spec.js
+--------------------------------------------------------------------------------
+  A arpa_subrecipients record in postgres looks like this:
+
+*/
+const { findRecipient } = requireSrc(__filename);
+const assert = require('assert');
+const _ = require('lodash');
+const { requestProviderMiddleware } = require('../../../../src/arpa_reporter/use-request');
+const { withTenantId } = require('../helpers/with-tenant-id');
+const knex = require('../../../../src/db/connection');
+
+const TENANT_A = 0;
+const TENANT_B = 1;
+const NONEXISTENT_TENANT = 100;
+
+describe('db/reporting-periods.js', () => {
+    const recipients = {
+        beneficiaryWithTIN: {
+            name: 'Beneficiary with TIN',
+            tin: 'TIN-1',
+            uei: null,
+        },
+        iaa: {
+            name: 'IAA',
+            tin: null,
+            uei: null,
+        },
+        contractorWithUEI: {
+            name: 'Contractor with UEI',
+            tin: null,
+            uei: 'UEI-1',
+        },
+    }
+    before(async () => {
+        await knex.raw('TRUNCATE TABLE arpa_subrecipients CASCADE');
+        await knex('arpa_subrecipients').insert(Object.values(recipients));
+    });
+    describe('findRecipient', () => {
+        it('Returns a recipient with UEI', async () => {
+            const result = await requestProviderMiddleware({ session: { user: { tenant_id: TENANT_A } } }, null, findRecipient('uei', 'UEI-1'));
+            console.log(result);
+            assert.equal(result.length, 21);
+        });
+    });
+});
+
+// NOTE: This file was copied from tests/server/db/reporting-periods.spec.js (git @ ada8bfdc98) in the arpa-reporter repo on 2022-09-23T20:05:47.735Z
--- packages/server/__tests__/arpa_reporter/server/services/validate-upload.spec.js
+++ packages/server/__tests__/arpa_reporter/server/services/validate-upload.spec.js
@@ -182,36 +182,75 @@ describe('validate record', () => {
 });
 
 describe('findRecipientInDatabase', () => {
-    const findRecipientInDatabase = validateUploadModule.__get__('findRecipientInDatabase');
-    const recipient = {
-        Unique_Entity_Identifier__c: 'UEI123',
-        EIN__c: 'EIN123',
-    };
-    const trns = {}; // mock transaction object
+    describe('non-IAA recipient', () => {
+        const findRecipientInDatabase = validateUploadModule.__get__('findRecipientInDatabase');
+        const beneficiaryRecipient = {
+            Name: 'Beneficiary Recipient',
+            Entity_Type_2__c: 'Beneficiary',
+            Unique_Entity_Identifier__c: 'UEI123',
+            EIN__c: 'EIN123',
+        };
+        const trns = {}; // mock transaction object
 
-    afterEach(() => {
-        sinon.restore();
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('should return the recipient found by UEI', async () => {
+            const mockFindRecipient = sinon.stub().withArgs('UEI123', null, trns).resolves({ name: 'John' });
+            validateUploadModule.__set__('findRecipient', mockFindRecipient);
+            const result = await findRecipientInDatabase({ beneficiaryRecipient, trns }, mockFindRecipient);
+            expect(result).to.deep.equal({ name: 'John' });
+        });
+
+        it('should return the recipient found by EIN', async () => {
+            const mockFindRecipient = sinon.stub().withArgs(null, 'EIN123', trns).resolves({ name: 'Jane' });
+            validateUploadModule.__set__('findRecipient', mockFindRecipient);
+            const result = await findRecipientInDatabase({ beneficiaryRecipient, trns }, mockFindRecipient);
+            expect(result).to.deep.equal({ name: 'Jane' });
+        });
+
+        it('should return null if recipient is not found', async () => {
+            const mockFindRecipient = sinon.stub().resolves(null);
+            validateUploadModule.__set__('findRecipient', mockFindRecipient);
+            const result = await findRecipientInDatabase({ beneficiaryRecipient, trns }, mockFindRecipient);
+            expect(result).to.be.null;
+        });
     });
+    describe('IAA recipients', () => {
+        const findRecipientInDatabase = validateUploadModule.__get__('findRecipientInDatabase');
+        const beneficiaryRecipient = {
+            Name: 'IAA Recipient',
+            Entity_Type_2__c: 'IAA',
+            Unique_Entity_Identifier__c: 'UEI123',
+            EIN__c: 'EIN123',
+        };
+        const trns = {}; // mock transaction object
 
-    it('should return the recipient found by UEI', async () => {
-        const mockFindRecipient = sinon.stub().withArgs('UEI123', null, trns).resolves({ name: 'John' });
-        validateUploadModule.__set__('findRecipient', mockFindRecipient);
-        const result = await findRecipientInDatabase({ recipient, trns }, mockFindRecipient);
-        expect(result).to.deep.equal({ name: 'John' });
-    });
+        afterEach(() => {
+            sinon.restore();
+        });
 
-    it('should return the recipient found by EIN', async () => {
-        const mockFindRecipient = sinon.stub().withArgs(null, 'EIN123', trns).resolves({ name: 'Jane' });
-        validateUploadModule.__set__('findRecipient', mockFindRecipient);
-        const result = await findRecipientInDatabase({ recipient, trns }, mockFindRecipient);
-        expect(result).to.deep.equal({ name: 'Jane' });
-    });
+        it('should return the recipient found by UEI', async () => {
+            const mockFindRecipient = sinon.stub().withArgs('uei', 'UEI123', trns).resolves({ name: 'John' });
+            validateUploadModule.__set__('findRecipient', mockFindRecipient);
+            const result = await findRecipientInDatabase({ beneficiaryRecipient, trns }, mockFindRecipient);
+            expect(result).to.deep.equal({ name: 'John' });
+        });
 
-    it('should return null if recipient is not found', async () => {
-        const mockFindRecipient = sinon.stub().resolves(null);
-        validateUploadModule.__set__('findRecipient', mockFindRecipient);
-        const result = await findRecipientInDatabase({ recipient, trns }, mockFindRecipient);
-        expect(result).to.be.null;
+        it('should return the recipient found by EIN', async () => {
+            const mockFindRecipient = sinon.stub().withArgs('tin', 'EIN123', trns).resolves({ name: 'Jane' });
+            validateUploadModule.__set__('findRecipient', mockFindRecipient);
+            const result = await findRecipientInDatabase({ beneficiaryRecipient, trns }, mockFindRecipient);
+            expect(result).to.deep.equal({ name: 'Jane' });
+        });
+
+        it('should return the recipient found by Name', async () => {
+            const mockFindRecipient = sinon.stub().withArgs('name', 'IAA Recipient', trns).resolves({ name: 'IAA Recipient' });
+            validateUploadModule.__set__('findRecipient', mockFindRecipient);
+            const result = await findRecipientInDatabase({ beneficiaryRecipient, trns }, mockFindRecipient);
+            expect(result).to.deep.equal({ name: 'IAA Recipient' });
+        });
     });
 });
 
