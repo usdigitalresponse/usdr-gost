95a7a28c20a1032f7940896a5bf57329278c7401
Aditya Sridhar <aditus06@gmail.com>
2024-09-19 17:20:14 -0400

    fix: update migrations to create a unique-index rather than a unique-constraint or plain index

--- packages/server/migrations/20240918183745_drop_constraints_to_arpa_subrecipients.js
+++ packages/server/migrations/20240918183745_drop_constraints_to_arpa_subrecipients.js
@@ -6,37 +6,6 @@ exports.up = async function (knex) {
     await knex.schema.alterTable('arpa_subrecipients', (table) => {
         table.dropUnique(['tenant_id', 'uei']);
         table.dropUnique(['tenant_id', 'tin']);
-
-        // table.index(
-        //     ['tenant_id', 'uei'],
-        //     'idx_arpa_subrecipients_tenant_id_uei_unique',
-        //     {
-        //         predicate: knex.whereNotNull('uei'),
-        //     },
-        // );
-        // table.index(
-        //     ['tenant_id', 'tin'],
-        //     'idx_arpa_subrecipients_tenant_id_tin_unique',
-        //     {
-        //         predicate: knex.whereNotNull('tin'),
-        //     },
-        // );
-        // table.unique(
-        //     ['tenant_id', 'uei'],
-        //     {
-        //         indexName: 'idx_arpa_subrecipients_tenant_id_uei_unique',
-        //         predicate: knex.whereNotNull('uei'),
-        //         useConstraint: false,
-        //     },
-        // );
-        // table.unique(
-        //     ['tenant_id', 'tin'],
-        //     {
-        //         indexName: 'idx_arpa_subrecipients_tenant_id_tin_unique',
-        //         predicate: knex.whereNotNull('tin'),
-        //         useConstraint: false,
-        //     },
-        // );
     });
     await knex.raw(
         'CREATE UNIQUE INDEX idx_arpa_subrecipients_tenant_id_uei_unique ON arpa_subrecipients (tenant_id, uei) WHERE uei IS NOT NULL',
@@ -52,13 +21,6 @@ exports.up = async function (knex) {
  */
 exports.down = function (knex) {
     return knex.schema.alterTable('arpa_subrecipients', (table) => {
-        // option 1
-        // table.dropIndex([], 'idx_arpa_subrecipients_tenant_id_tin_unique');
-        // table.dropIndex([], 'idx_arpa_subrecipients_tenant_id_uei_unique');
-        // option 2
-        // table.dropUnique([], 'idx_arpa_subrecipients_tenant_id_tin_unique');
-        // table.dropUnique([], 'idx_arpa_subrecipients_tenant_id_uei_unique');
-        // option 3
         table.dropIndex([], 'idx_arpa_subrecipients_tenant_id_tin_unique');
         table.dropIndex([], 'idx_arpa_subrecipients_tenant_id_uei_unique');
 
--- packages/server/migrations/20240918183915_add_name_to_arpa_subrecipients.js
+++ packages/server/migrations/20240918183915_add_name_to_arpa_subrecipients.js
@@ -5,24 +5,6 @@
 exports.up = async function (knex) {
     await knex.schema.table('arpa_subrecipients', (table) => {
         table.text('name');
-        // option 1
-        // table.index(
-        //     ['tenant_id', 'name'],
-        //     'idx_arpa_subrecipients_tenant_id_name_unique',
-        //     {
-        //         predicate: knex.whereRaw('name is not null and uei is null and tin is null'),
-        //     },
-        // );
-        // option 2
-        // table.unique(
-        //     ['tenant_id', 'name'],
-        //     {
-        //         indexName: 'idx_arpa_subrecipients_tenant_id_name_unique',
-        //         predicate: knex.whereRaw('name is not null and uei is null and tin is null'),
-        //         useConstraint: false,
-        //     },
-        // );
-        // option 3
     });
     await knex.raw(
         'CREATE UNIQUE INDEX idx_arpa_subrecipients_tenant_id_name_unique ON arpa_subrecipients (tenant_id, name) WHERE name is not null and uei is null and tin is null',
@@ -35,11 +17,6 @@ exports.up = async function (knex) {
  */
 exports.down = function (knex) {
     return knex.schema.table('arpa_subrecipients', (table) => {
-        // option 1
-        // table.dropIndex([], 'idx_arpa_subrecipients_tenant_id_name_unique');
-        // option 2
-        // table.dropUnique([], 'idx_arpa_subrecipients_tenant_id_name_unique');
-        // option 3
         table.dropIndex([], 'idx_arpa_subrecipients_tenant_id_name_unique');
 
         table.dropColumn('name');
