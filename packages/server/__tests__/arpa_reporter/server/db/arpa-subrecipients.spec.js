4a51e0b8e1d7bda49c42bda909b0680d404f0bb5
Aditya Sridhar <aditus06@gmail.com>
2024-09-19 17:21:22 -0400

    fix: remove unnecessary comment

--- packages/server/__tests__/arpa_reporter/server/db/arpa-subrecipients.spec.js
+++ packages/server/__tests__/arpa_reporter/server/db/arpa-subrecipients.spec.js
@@ -238,5 +238,3 @@ describe('db/arpa-subrecipients.js', () => {
         });
     });
 });
-
-// NOTE: This file was copied from tests/server/db/reporting-periods.spec.js (git @ ada8bfdc98) in the arpa-reporter repo on 2022-09-23T20:05:47.735Z
