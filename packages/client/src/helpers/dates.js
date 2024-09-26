4ee1a006d439d2cf75a3fa4e1e93e579e298cf6c
greg-adams <greg@codescience.com>
2024-09-26 15:02:59 -0700

    remove unused

--- packages/client/src/helpers/dates.js
+++ packages/client/src/helpers/dates.js
@@ -12,5 +12,3 @@ export function daysUntil(dateString) {
 export const formatDate = (dateString) => DateTime.fromISO(dateString).toLocaleString(DateTime.DATE_MED);
 
 export const formatDateTime = (dateString) => DateTime.fromISO(dateString).toLocaleString(DateTime.DATETIME_MED);
-
-export const ISO_DATE = 'YYYY-MM-DD';
