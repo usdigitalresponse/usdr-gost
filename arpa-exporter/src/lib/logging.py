801bbb18c08f9b7d4e7a71c5be82323d7612551d
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2025-01-18 00:01:32 +0000

    (formatting) Configure datadog logs injection

--- arpa-exporter/src/lib/logging.py
+++ arpa-exporter/src/lib/logging.py
@@ -9,20 +9,21 @@ import structlog
 
 
 def datadog_tracer_injection(logger, log_method, event_dict):
-    """See https://docs.datadoghq.com/tracing/other_telemetry/connect_logs_and_traces/python/
-    """
+    """See https://docs.datadoghq.com/tracing/other_telemetry/connect_logs_and_traces/python/"""
     # get correlation ids from current tracer context
     span = tracer.current_span()
-    trace_id, span_id = (str((1 << 64) - 1 & span.trace_id), span.span_id) if span else (None, None)
+    trace_id, span_id = (
+        (str((1 << 64) - 1 & span.trace_id), span.span_id) if span else (None, None)
+    )
 
     # add ids to structlog event dictionary
-    event_dict['dd.trace_id'] = str(trace_id or 0)
-    event_dict['dd.span_id'] = str(span_id or 0)
+    event_dict["dd.trace_id"] = str(trace_id or 0)
+    event_dict["dd.span_id"] = str(span_id or 0)
 
     # add the env, service, and version configured for the tracer
-    event_dict['dd.env'] = ddtrace.config.env or ""
-    event_dict['dd.service'] = ddtrace.config.service or ""
-    event_dict['dd.version'] = ddtrace.config.version or ""
+    event_dict["dd.env"] = ddtrace.config.env or ""
+    event_dict["dd.service"] = ddtrace.config.service or ""
+    event_dict["dd.version"] = ddtrace.config.version or ""
 
     return event_dict
 
