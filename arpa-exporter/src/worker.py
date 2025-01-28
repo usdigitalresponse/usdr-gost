229397375e3b754c96616936713bda62d5281963
Aditya Sridhar <aditus06@gmail.com>
2025-01-28 11:37:34 -0500

    feat: add schema for messages and source-paths. add metadata-file to the archive. update build_zip to match latest variable names

--- arpa-exporter/src/worker.py
+++ arpa-exporter/src/worker.py
@@ -3,6 +3,9 @@ import os
 import signal
 import tempfile
 import zipfile
+from typing import List, Dict
+from pydantic import BaseModel
+import csv
 
 import boto3
 import botocore.client
@@ -13,6 +16,29 @@ from src.lib.logging import get_logger, reset_contextvars
 
 
 TASK_QUEUE_URL = os.environ["TASK_QUEUE_URL"]
+DATA_DIR = os.getenv('DATA_DIR', 'arpa-exporter/src/data')
+METADATA_DIR = os.path.join(DATA_DIR, 'archive_metadata')
+
+
+class UploadInfo(BaseModel):
+    upload_id: str
+    filename_in_zip: str
+    directory_location: str
+    agency_name: str
+    ec_code: str
+    reporting_period_name: str
+    validity: str
+
+
+class S3Schema(BaseModel):
+    bucket: str
+    key: str
+
+
+class MessageSchema(BaseModel):
+    s3: S3Schema
+    organization_id: int
+    metadata_filename: str
 
 
 class ShutdownHandler:
@@ -32,47 +58,51 @@ class ShutdownHandler:
         return self._shutdown_requested
 
 
-def get_entry_path(source_path):
-    # TODO: return the path to where the source file should go in the archive
-    return source_path
-
-
-def build_zip(fh, source_paths):
-    # TODO: for each source file:
-    #   1. figure out its destination path for the archive
-    #   2. if the archive does not already contain an entry at the destination path, add it
+def build_zip(fh, source_paths: List[UploadInfo]):
     logger = get_logger(total_source_files=len(source_paths))
     files_added = 0
-
-    with zipfile.ZipFile(fh, "a") as archive:
-        for source_path in source_paths:
-            entry_path = get_entry_path(source_path)
-            entry_logger = logger.bind(source_path=source_path, entry_path=entry_path)
-            if entry_path not in archive.namelist():
+    with zipfile.ZipFile(fh, 'a') as zipf:
+        for upload in source_paths:
+            entry_logger = logger.bind(source_path=os.path.join(DATA_DIR, f'{upload.upload_id}.xlsm'), entry_path=upload.directory_location)
+            if upload.directory_location in zipf.namelist():
+                entry_logger.info("file already exists in archive")
+            else:
                 try:
-                    archive.write(source_path, arcname=entry_path)
+                    zipf.write(os.path.join(DATA_DIR, f'{upload.upload_id}.xlsm'), arcname=upload.directory_location)
                 except:
                     entry_logger.exception(
                         "error writing source file to entry in archive"
                     )
                     raise
                 files_added += 1
-                entry_logger.info("added file to archive")
-            else:
-                # TODO should we log that it already exists?
-                entry_logger.info("file already exists in archive")
+                entry_logger.info("Added file to the archive.")
 
     logger.info("updated zip archive", files_added=files_added)
 
 
-def process_sqs_message_request(s3, message_data, local_file):
+def add_metadata_csv_to_zip(fh, metadata_filepath: str, metadata_filename: str):
+    with zipfile.ZipFile(fh, 'a') as zipf:
+        zipf.write(metadata_filepath, arcname=f'/metadata/{metadata_filename}')
+
+
+def load_source_paths_from_csv(metadata_filepath: str):
+    with open(metadata_filepath, 'r') as f:
+        reader = csv.DictReader(f)
+        return [UploadInfo(**row) for row in reader]
+
+
+def get_metadata_filepath(organization_id: int, metadata_filename: str):
+    return os.path.join(METADATA_DIR, organization_id, metadata_filename)
+
+
+def process_sqs_message_request(s3, message_data: MessageSchema, local_file):
     # Get the S3 object if it already exists (if 404, assume it doesn't & create from scratch)
     # Note:
     #   Fargate ephemeral storage is free up to 20GB, so we should download and/or build
     #   the S3 file using tempfile storage. We have a good pattern for doing that using context
     #   managers in CPF Reporter.
-    s3_bucket = message_data["s3"]["bucket"]
-    s3_key = message_data["s3"]["key"]
+    s3_bucket = message_data.s3.bucket
+    s3_key = message_data.s3.key
 
     try:
         s3.download_fileobj(s3_bucket, s3_key, local_file)
@@ -81,13 +111,30 @@ def process_sqs_message_request(s3, message_data, local_file):
             get_logger().exception("error downloading S3 object")
             raise
 
-    # TODO: use message_data to determine source files that should be in the archive
+    metadata_filepath = get_metadata_filepath(message_data.organization_id, message_data.metadata_filename)
+
+    # Step 1 - Load all the source paths from the metadata CSV file
     try:
-        build_zip(local_file, source_paths=message_data["sources"])
+        source_paths = load_source_paths_from_csv(metadata_filepath)
+    except:
+        get_logger().exception("error loading source paths from CSV")
+        raise
+
+    # Step 2 - Add the metadata CSV to the zip archive
+    try:
+        add_metadata_csv_to_zip(local_file, metadata_filepath, message_data.metadata_filename)
+    except:
+        get_logger().exception("error adding metadata CSV to zip archive")
+        raise
+
+    # Step 3 - Add any files listed in the metadata CSV that are not already in the zip archive
+    try:
+        build_zip(local_file, source_paths=source_paths)
     except:
         get_logger().exception("error building zip archive")
         raise
 
+    # Step 4 - Upload the zip archive back to S3
     try:
         local_file.seek(0)
         s3.upload_fileobj(local_file, s3_bucket, s3_key)
@@ -118,17 +165,23 @@ def handle_work(s3, sqs):
     )
     logger.info("received message from SQS")
     try:
-        data = json.loads(message["Body"])
+        raw_data = json.loads(message["Body"])
     except json.JSONDecodeError:
         # This is a problem with the message, not the worker, so don't re-raise
         logger.exception("error parsing request data from SQS message")
         return
 
+    try:
+        data = MessageSchema(**raw_data)
+    except:
+        logger.exception("SQS message data did not match expected schema")
+        raise
+
     with tempfile.NamedTemporaryFile() as tfh:
         try:
             with structlog.contextvars.bound_contextvars(
-                s3_bucket=data["s3"]["bucket"],
-                s3_key=data["s3"]["key"],
+                s3_bucket=data.s3.bucket,
+                s3_key=data.s3.key,
                 destination_file_path=tfh.name,
                 destination_file_mode=tfh.mode,
             ):
