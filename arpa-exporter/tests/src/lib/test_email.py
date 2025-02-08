7465cd8a489fe0f7d914cdb9a67d88e7e0a12107
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2025-02-08 01:42:07 +0000

    Expand test coverage for src.lib.email module

--- arpa-exporter/tests/src/lib/test_email.py
+++ arpa-exporter/tests/src/lib/test_email.py
@@ -1,45 +1,42 @@
+from __future__ import annotations
+
 import os
-from moto import mock_aws
-import structlog
-import boto3
+import typing
+import urllib
 
 from src.lib import email
+
+if typing.TYPE_CHECKING:
+    import moto.ses.models
 
 
 class TestEmail:
     def test_generate_email(self):
+        test_download_url = "https://example.org/your/download/is/here.zip"
         email_html, email_text, subject = email.generate_email(
-            logger=structlog.get_logger(),
-            url="https://example.com",
+            download_url=test_download_url,
         )
-        assert "https://example.com" in email_html
-        assert "https://example.com" in email_text
+        assert test_download_url in email_text
+        assert urllib.parse.quote(test_download_url) in email_html
         assert "USDR Full File Export" == subject
 
-    @mock_aws
-    def test_send_email(self):
-        os.environ["NOTIFICATIONS_EMAIL"] = "grants-notifications@usdigitalresponse.org"
-        region = "us-west-2"
-
-        email_client = boto3.client("ses", region_name=region)
-
-        # Verify the email address otherwise SES will throw an error
-        email_client.verify_email_address(EmailAddress=os.getenv("NOTIFICATIONS_EMAIL"))
-
+    def test_send_email(self, ses, ses_sent_messages: list[moto.ses.models.Message]):
         email_html = "Test"
         email_text = "Test"
         subject = "Test"
         user_email = "foo@example.com"
-        logger = structlog.get_logger()
 
-        email.send_email(
-            email_client=email_client,
+        message_id = email.send_email(
+            email_client=ses,
             dest_email=user_email,
             email_html=email_html,
             email_text=email_text or "",
             subject=subject or "",
-            logger=logger,
         )
-        send_quota = email_client.get_send_quota()
-        sent_count = int(send_quota["SentLast24Hours"])
-        assert sent_count == 1
+
+        assert len(ses_sent_messages) == 1
+        sent_message = ses_sent_messages[0]
+        assert sent_message.id == message_id
+        assert sent_message.body == email_html
+        assert sent_message.source == os.environ["NOTIFICATIONS_EMAIL"]
+        assert sent_message.destinations["ToAddresses"] == [user_email]
