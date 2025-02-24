5b015ab8bd0e79d10d5700f3cbe95ff9ede40580
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2025-02-24 17:09:36 +0000

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -319,8 +319,8 @@ module "arpa_audit_report" {
     access_point_id = module.api.efs_data_volume_access_point_id
   }]
   additional_task_role_json_policies = {
-    rw-audit-reports-bucket   = data.aws_iam_policy_document.arpa_audit_report_rw_reports_bucket.json
-    send-emails               = module.api.send_emails_policy_json
+    rw-audit-reports-bucket = data.aws_iam_policy_document.arpa_audit_report_rw_reports_bucket.json
+    send-emails             = module.api.send_emails_policy_json
   }
 
   # Task resource configuration
